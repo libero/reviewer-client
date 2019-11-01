e4c905a4fde1eac08fbc41e5fb4ecb31ed2d4e78
Cory <c.donkin@elifesciences.org>
2019-11-01 14:32:59 +0000

    Add blank story for NameEmail component

---
+++ src/ui/stories/molecules/NameEmail.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered/react';
+
+storiesOf('ui | molecules/NameEmail', module)
+    .addDecorator(centered)
+    .add(
+        'NameEmail',
+        (): JSX.Element => {
+            return <div>Email and Name component</div>;
+        },
+    );
