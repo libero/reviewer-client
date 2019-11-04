0b9ef4704bffbd1a3d06e994314389d37d69c7a0
Cory <c.donkin@elifesciences.org>
2019-11-04 10:03:59 +0000

    add NavBar test file

---
+++ src/core/components/NavBar.test.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { render, RenderResult, fireEvent } from '@testing-library/react';
+import routerWrapper from '../../../test-utils/routerWrapper';
+import NavBar from './NavBar';
+
+const expectedMenuItems = [
+    {
+        display: 'navbar.dashboard',
+        url: '/',
+    },
+    {
+        display: 'navbar.author-guide',
+        url: '/author-guide',
+    },
+    {
+        display: 'navbar.reviewer-guide',
+        url: '/reviewer-guide',
+    },
+    {
+        display: 'navbar.contact-us',
+        url: '/contact-us',
+    },
+];
+
+describe('NavBar', (): void => {
+    it('should render correctly', (): void => {
+        expect((): RenderResult => render(<NavBar />, { wrapper: routerWrapper() })).not.toThrow();
+    });
+
+    it('should render a menu with expected items', (): void => {
+        const { container } = render(<NavBar />, { wrapper: routerWrapper() });
+        for (let item of expectedMenuItems) {
+            const element = container.querySelector('.menu__link[href="' + item.url + '"]');
+            expect(element).toBeInTheDocument();
+            expect(element.textContent).toBe(item.display);
+        }
+    });
+    it('should render a burger menu with expected items', (): void => {
+        const { container } = render(<NavBar />, { wrapper: routerWrapper() });
+        fireEvent.click(container.querySelector('.burger_menu button'));
+
+        for (let item of expectedMenuItems) {
+            const element = container.querySelector('.burger_menu__link[href="' + item.url + '"]');
+            expect(element).toBeInTheDocument();
+            expect(element.textContent).toBe(item.display);
+        }
+    });
+});
