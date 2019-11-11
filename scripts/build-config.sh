41a64f9b673ba5a3251934fc0367a840308fd8de
Peter Hooper <diversemix@gmail.com>
2019-11-11 15:25:19 +0000

    take out debug out of conf file

--- scripts/build-config.sh
+++ scripts/build-config.sh
@@ -2,6 +2,5 @@
 envsubst ' $${CLIENT_PORT} $${SERVER_PORT} $${CLIENT_API_PROXY_URL} ' \
   < /etc/nginx/nginx.conf.template \
   > /etc/nginx/nginx.conf \
-  && cat /etc/nginx/nginx.conf \
   && nginx -g 'daemon off;'
 
