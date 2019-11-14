134f7f58e292987b647e08135f5199f8b779e86a
Erez Odier <erez.odier@gmail.com>
2019-11-14 10:59:31 +0000

    fixed build-config script for env var substitution in config file

--- scripts/build-config.sh
+++ scripts/build-config.sh
@@ -5,7 +5,9 @@ CONFIG_FILENAME=$(find /usr/share/nginx/html/ -name "config.*.js")
 envsubst ' $${CLIENT_PORT} $${SERVER_PORT} $${CLIENT_API_PROXY_URL} ' \
   < /etc/nginx/nginx.conf.template \
   > /etc/nginx/nginx.conf \
+  && cp $CONFIG_FILENAME config-temp.js \
   && envsubst ' $${CONTINUUM_LOGIN_URL} $${CONTINUUM_LOGIN_PORT} $${CLIENT_PORT} $${CLIENT_API_URL} ' \
-  < $CONFIG_FILENAME \
+  < config-temp.js \
   > $CONFIG_FILENAME \
+  && rm config-temp.js \
   && nginx -g 'daemon off;'
