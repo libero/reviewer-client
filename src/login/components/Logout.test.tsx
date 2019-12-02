81b0f7eb7bf34ca8fa8ac12c850a892f48873125
erezmus <e.odier@elifesciences.org>
2019-12-02 14:54:39 +0000

    * Completed #38: added Logout component
    * connected logout nav link
    * use docker-compose urls

---
+++ src/login/components/Logout.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, cleanup, RenderResult } from '@testing-library/react';
+import * as tokenUtils from '../utils/tokenUtils';
+import Logout from './Logout';
+import { MemoryRouter, Route } from 'react-router';
+
+describe('Logout', () => {
+    afterEach(cleanup);
+
+    it('should render correctly', (): void => {
+        expect(
+            (): RenderResult =>
+                render(
+                    <MemoryRouter initialEntries={['/logout']}>
+                        <Logout />
+                    </MemoryRouter>,
+                ),
+        ).not.toThrow();
+    });
+
+    it('should clear the token', (): void => {
+        jest.spyOn(tokenUtils, 'clearToken').mockImplementationOnce(() => {});
+
+        render(
+            <MemoryRouter initialEntries={['/logout']}>
+                <Logout />
+            </MemoryRouter>,
+        );
+
+        expect(tokenUtils.clearToken).toHaveBeenCalledTimes(1);
+    });
+
+    it('should redirect to the root route', (): void => {
+        const { container } = render(
+            <MemoryRouter initialEntries={['/logout']}>
+                <Route component={Logout} exact path="/logout"></Route>
+                <Route exact path="/" render={(): string => 'Root'} />
+            </MemoryRouter>,
+        );
+
+        expect(container.textContent).toBe('Root');
+    });
+});
