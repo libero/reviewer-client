2d8d096fcf960940df24abf52d398d12ea301ce1
Will Byrne <w.byrne@elifesciences.org>
2019-12-09 13:38:03 +0000

    fix broken test

--- tests/page-objects/DashboardPage.ts
+++ tests/page-objects/DashboardPage.ts
@@ -1,7 +1,7 @@
 const dashboardCommands = {
     onPage: async function(): Promise<void> {
         await this.waitForElementVisible('@newSubmissionsButton', 10000);
-        await this.assert.containsText('@newSubmissionsButton', 'NEW SUBMISSION');
+        await this.assert.containsText('@newSubmissionsButton', 'START A NEW SUBMISSION');
         await this.assert.containsText('@menuLink', 'Dashboard');
     },
 };
@@ -10,7 +10,7 @@ export = {
     commands: [dashboardCommands],
     elements: {
         newSubmissionsButton: {
-            selector: '.dashboard__button_container',
+            selector: '.no-submissions__buttons',
         },
         menuLink: {
             selector: '.menu__link--active',
