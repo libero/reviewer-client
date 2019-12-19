7f9d7d09923ce529fd2fe04cd62387bcc87ff132
Cory <c.donkin@elifesciences.org>
2019-12-19 10:04:03 +0000

    add act error suppression to prevent console error output

--- src/dashboard/components/Dashboard.test.tsx
+++ src/dashboard/components/Dashboard.test.tsx
@@ -8,6 +8,7 @@ import apolloWrapper from '../../../test-utils/apolloWrapper';
 import Dashboard from './Dashboard';
 import { getSubmissionsQuery } from '../graphql';
 import { MockedResponse } from '@apollo/react-testing';
+import { debug } from 'webpack';
 const generateMockQueryResponse = (subs: Submission[]): MockedResponse[] => {
     return [
         {
@@ -25,6 +26,20 @@ const generateMockQueryResponse = (subs: Submission[]): MockedResponse[] => {
 
 describe('Dashboard', (): void => {
     afterEach(cleanup);
+    const originalError = console.error;
+    beforeAll(() => {
+        // This is horrible but necessary to prevent console error output which isn't to do with the test scenarios see: https://github.com/libero/reviewer-client/issues/69
+        console.error = (...args: unknown[]) => {
+            if (/Warning.*not wrapped in act/.test(args[0] as string)) {
+                return;
+            }
+            originalError.call(console, ...args);
+        };
+    });
+
+    afterAll(() => {
+        console.error = originalError;
+    });
 
     it('should render correctly', (): void => {
         expect(
@@ -36,13 +51,13 @@ describe('Dashboard', (): void => {
     });
 
     it('should render the no-submissions page if there are no submissions', async (): Promise<void> => {
-        const { findByText } = render(<Dashboard />, {
+        const { container, debug } = render(<Dashboard />, {
             wrapper: combineWrappers(apolloWrapper(generateMockQueryResponse([])), routerWrapper(['/link-1'])),
         });
         // flush graphql fetch promise
-        await setTimeout(() => {});
-        // const testElement = await findByText('new-system-1');
-        // expect(testElement).toBeInTheDocument();
+        await new Promise(resolve => setTimeout(resolve));
+        const testElement = await container.querySelector('.no-submissions');
+        expect(testElement).toBeInTheDocument();
     });
 
     it('should render the standard dashboard page if there are submissions', async (): Promise<void> => {
@@ -51,12 +66,12 @@ describe('Dashboard', (): void => {
             title: 'Effects of Caffeine on Software Developers',
             updated: Date.now(),
         };
-        const { findByText } = render(<Dashboard />, {
+        const { container } = render(<Dashboard />, {
             wrapper: combineWrappers(apolloWrapper(generateMockQueryResponse([sampleSub])), routerWrapper(['/link-1'])),
         });
         // flush graphql fetch promise
-        await setTimeout(() => {});
-        // const testElement = await findByText('Submissions');
-        // expect(testElement).toBeInTheDocument();
+        await new Promise(resolve => setTimeout(resolve));
+        const testElement = await container.querySelector('.dashboard');
+        expect(testElement).toBeInTheDocument();
     });
 });
