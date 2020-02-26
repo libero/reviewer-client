d9a99c1c2a41fe48b69cb156fc675b8ce2656b2d
Cory <c.donkin@elifesciences.org>
2020-02-26 09:17:34 +0000

    begin error state work

--- src/ui/molecules/FileUpload.tsx
+++ src/ui/molecules/FileUpload.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, Fragment } from 'react';
 import { useDropzone, DropEvent } from 'react-dropzone';
 import { useTranslation } from 'react-i18next';
 import UploadProgress from './UploadProgress';
@@ -17,19 +17,36 @@ const FileUpload: React.FC<Props> = ({ onUpload, progress, status }: Props): JSX
     return (
         <div className="file-upload">
             <div
-                className={`file-upload__dropzone ${isDragActive ? 'file-upload__dropzone--drag-active' : ''}`}
+                className={`file-upload__dropzone file-upload__dropzone--${status.toLowerCase()} ${
+                    isDragActive ? 'file-upload__dropzone--drag-active' : ''
+                }`}
                 {...getRootProps()}
             >
                 <input {...getInputProps()} />
                 <UploadProgress progress={progress} status={status} />
                 <div className="file-upload__content">
-                    <span className="typography__body">
-                        <button onClick={open} className="typography__body--button-link">
-                            {t('ui:file-upload.idle-upload')}
-                        </button>
-                        {t('ui:file-upload.idle-content')}
+                    <span className="typography__body file-upload__description">
+                        {status === 'ERROR' ? (
+                            <Fragment>
+                                <span className="file-upload__error-prefix">
+                                    {t('ui:file-upload.error-pre-content')}
+                                </span>
+                                Some dynamic message relating to error state. Please{' '}
+                                <button onClick={open} className="typography__body--button-link">
+                                    {t('ui:file-upload.error-upload')}
+                                </button>
+                                .
+                            </Fragment>
+                        ) : (
+                            <Fragment>
+                                <button onClick={open} className="typography__body--button-link">
+                                    {t('ui:file-upload.idle-upload')}
+                                </button>
+                                {t('ui:file-upload.idle-content')}
+                            </Fragment>
+                        )}
                     </span>
-                    <span className="typography__small typography__small--secondary">
+                    <span className="typography__small typography__small--secondary file-upload__extra">
                         {t('ui:file-upload.idle-description')}
                     </span>
                 </div>
