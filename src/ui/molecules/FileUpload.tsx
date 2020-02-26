fb516edefb91b06d93c9900ff4319d1212f8bebe
Cory <c.donkin@elifesciences.org>
2020-02-26 14:27:20 +0000

    refactor render mess to be switch case in content component

--- src/ui/molecules/FileUpload.tsx
+++ src/ui/molecules/FileUpload.tsx
@@ -19,8 +19,98 @@ interface Props {
     };
 }
 
-const FileUpload: React.FC<Props> = ({ onUpload, state }: Props): JSX.Element => {
+interface FileUploadContentProps {
+    status: 'IDLE' | 'UPLOADING' | 'COMPLETE' | 'ERROR';
+    open: () => void;
+    error?: 'multiple' | 'size' | 'server';
+    uploadInProgress?: {
+        progress?: number;
+        fileName?: string;
+    };
+    fileStored?: {
+        fileName?: string;
+        previewLink?: string;
+    };
+}
+const FileUploadContent = ({
+    status,
+    open,
+    error,
+    uploadInProgress = {},
+    fileStored = {},
+}: FileUploadContentProps): JSX.Element => {
     const { t } = useTranslation();
+    switch (status) {
+        case 'ERROR':
+            return (
+                <Fragment>
+                    <span className="typography__body file-upload__description">
+                        <span className="file-upload__error-prefix">{t('ui:file-upload.error-pre-content')}</span>
+                        {t('ui:file-upload.error-message.' + error)} Please{' '}
+                        <button onClick={open} className="typography__body--button-link">
+                            {t('ui:file-upload.error-upload')}
+                        </button>
+                        .
+                    </span>
+                    <span className="typography__small typography__small--secondary file-upload__extra">
+                        {t('ui:file-upload.error-extra.' + error)}
+                        {error === 'server' ? (
+                            <Link to="/contact-us" className="typography__body--link">
+                                {t('ui:file-upload.error-extra.contact-us') + '.'}
+                            </Link>
+                        ) : null}
+                    </span>
+                </Fragment>
+            );
+        case 'UPLOADING':
+            return (
+                <Fragment>
+                    <span className="typography__body file-upload__description">
+                        {t('ui:file-upload.uploading-message')}
+                        <span className="file-upload__progress-percentage">{uploadInProgress.progress}%</span>
+                    </span>
+                    <span className="typography__small typography__small--secondary file-upload__extra">
+                        {uploadInProgress.fileName}
+                    </span>
+                </Fragment>
+            );
+        case 'COMPLETE':
+            return (
+                <Fragment>
+                    <span className="typography__body file-upload__description">
+                        {t('ui:file-upload.complete-prefix')}
+                        <a className="typography__body--link" href={fileStored.previewLink}>
+                            {t('ui:file-upload.complete-preview')}
+                        </a>
+                        {t('ui:file-upload.complete-message')}
+                        <button onClick={open} className="typography__body--button-link">
+                            {t('ui:file-upload.complete-replace')}
+                        </button>
+                        {t('ui:file-upload.complete-suffix')}
+                    </span>
+                    <span className="typography__small typography__small--secondary file-upload__extra">
+                        {fileStored.fileName}
+                    </span>
+                </Fragment>
+            );
+        default:
+            return (
+                <Fragment>
+                    <span className="typography__body file-upload__description">
+                        <button onClick={open} className="typography__body--button-link">
+                            {t('ui:file-upload.idle-upload')}
+                        </button>
+                        {t('ui:file-upload.idle-message')}
+                    </span>{' '}
+                    <span className="typography__small typography__small--secondary file-upload__extra">
+                        {t('ui:file-upload.idle-extra')}
+                    </span>
+                </Fragment>
+            );
+    }
+};
+
+const FileUpload: React.FC<Props> = ({ onUpload, state }: Props): JSX.Element => {
     const onDrop = useCallback(onUpload, []);
     const { getRootProps, getInputProps, open, isDragActive } = useDropzone({ onDrop, noClick: true });
 
@@ -50,69 +140,13 @@ const FileUpload: React.FC<Props> = ({ onUpload, state }: Props): JSX.Element =>
                 <input {...getInputProps()} />
                 <UploadProgress progress={state.uploadInProgress && state.uploadInProgress.progress} status={status} />
                 <div className="file-upload__content">
-                    {status === 'ERROR' ? (
-                        <Fragment>
-                            <span className="typography__body file-upload__description">
-                                <span className="file-upload__error-prefix">
-                                    {t('ui:file-upload.error-pre-content')}
-                                </span>
-                                {t('ui:file-upload.error-message.' + state.error)} Please{' '}
-                                <button onClick={open} className="typography__body--button-link">
-                                    {t('ui:file-upload.error-upload')}
-                                </button>
-                                .
-                            </span>
-                            <span className="typography__small typography__small--secondary file-upload__extra">
-                                {t('ui:file-upload.error-extra.' + state.error)}
-                                {state.error === 'server' ? (
-                                    <Link to="/contact-us" className="typography__body--link">
-                                        {t('ui:file-upload.error-extra.contact-us') + '.'}
-                                    </Link>
-                                ) : null}
-                            </span>
-                        </Fragment>
-                    ) : status === 'UPLOADING' ? (
-                        <Fragment>
-                            <span className="typography__body file-upload__description">
-                                {t('ui:file-upload.uploading-message')}
-                                <span className="file-upload__progress-percentage">
-                                    {state.uploadInProgress ? state.uploadInProgress.progress : 0}%
-                                </span>
-                            </span>
-                            <span className="typography__small typography__small--secondary file-upload__extra">
-                                {state.uploadInProgress.fileName}
-                            </span>
-                        </Fragment>
-                    ) : status === 'COMPLETE' ? (
-                        <Fragment>
-                            <span className="typography__body file-upload__description">
-                                {t('ui:file-upload.complete-prefix')}
-                                <a className="typography__body--link" href={state.fileStored.previewLink}>
-                                    {t('ui:file-upload.complete-preview')}
-                                </a>
-                                {t('ui:file-upload.complete-message')}
-                                <button onClick={open} className="typography__body--button-link">
-                                    {t('ui:file-upload.complete-replace')}
-                                </button>
-                                {t('ui:file-upload.complete-suffix')}
-                            </span>
-                            <span className="typography__small typography__small--secondary file-upload__extra">
-                                {state.fileStored.fileName}
-                            </span>
-                        </Fragment>
-                    ) : (
-                        <Fragment>
-                            <span className="typography__body file-upload__description">
-                                <button onClick={open} className="typography__body--button-link">
-                                    {t('ui:file-upload.idle-upload')}
-                                </button>
-                                {t('ui:file-upload.idle-message')}
-                            </span>{' '}
-                            <span className="typography__small typography__small--secondary file-upload__extra">
-                                {t('ui:file-upload.idle-extra')}
-                            </span>
-                        </Fragment>
-                    )}
+                    <FileUploadContent
+                        status={status}
+                        open={open}
+                        error={state.error}
+                        uploadInProgress={state.uploadInProgress}
+                        fileStored={state.fileStored}
+                    />
                 </div>
             </div>
         </div>
