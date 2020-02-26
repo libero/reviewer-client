baa644126c1fdb3f04ee27512240c0585cbe5a2b
erezmus <e.odier@elifesciences.org>
2020-02-26 09:23:57 +0000

    Basic cover letter component

---
+++ src/ui/molecules/CoverLetter.tsx
@@ -0,0 +1,28 @@
+import React, { TextareaHTMLAttributes } from 'react';
+import { Close } from '@material-ui/icons';
+
+interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+    helperText?: string;
+    id: string;
+    invalid?: boolean;
+    register?: () => void;
+    className?: string;
+}
+
+const CoverLetter = ({ id, className, invalid, register, helperText, ...rest }: Props): JSX.Element => {
+    return (
+        <div className={`cover-letter${className ? ' ' + className : ''}`}>
+            <textarea className="cover-letter__input" id={id} name={id} ref={register} {...rest}></textarea>
+            <span
+                className={`typography__label typography__label--helper-text ${
+                    invalid ? 'typography__label--error' : 'typography__label--secondary'
+                }`}
+            >
+                {invalid && <Close fontSize="small" />}
+                {helperText}
+            </span>
+        </div>
+    );
+};
+
+export default CoverLetter;
---
+++ src/ui/stories/molecules/CoverLetter.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { CoverLetter } from '../../molecules';
+
+storiesOf('ui | molecules/CoverLetter', module).add(
+    'CoverLetter',
+    (): JSX.Element => {
+        return <CoverLetter id="someid" placeholder="Enter text here" />;
+    },
+);
