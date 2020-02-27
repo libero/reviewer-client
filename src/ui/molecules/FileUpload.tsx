bf6b2d1e985bbbbd86aac5d133c00e9a1b87eebd
Cory <c.donkin@elifesciences.org>
2020-02-27 13:21:15 +0000

    127 - fileupload states (#132)
    * forward progress and status through fileupload to progress component
    
    * add idle border styling and drag state
    
    * begin adding description text to idle state
    
    * begin error state work
    
    * add status properties
    
    * add inprogress state
    
    * upload complete state on file upload
    
    * spacing tweaks
    
    * refactor render mess to be switch case in content component
    
    * factorize some of the types
    
    * start re-enabling fileupload tests
    
    * update tests for new file upload logic
    
    * border swtyling fixes
    
    * sass linting
    
    Co-authored-by: Will Byrne <will.alex.byrne@gmail.com>

--- src/ui/molecules/FileUpload.tsx
+++ src/ui/molecules/FileUpload.tsx
@@ -1,24 +1,151 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import { useDropzone, DropEvent } from 'react-dropzone';
 import { useTranslation } from 'react-i18next';
 import UploadProgress from './UploadProgress';
 
-interface Props {
-    onUpload<T extends File>(acceptedFiles: T[], rejectedFiles: T[], event: DropEvent): void;
+type UploadErrors = 'multiple' | 'size' | 'server';
+type UploadInProgress = {
+    progress?: number;
+    fileName?: string;
+};
+type FileStored = {
+    fileName?: string;
+    previewLink?: string;
+};
+
+interface FileUploadContentProps {
+    status: 'IDLE' | 'UPLOADING' | 'COMPLETE' | 'ERROR';
+    open: () => void;
+    error?: UploadErrors;
+    uploadInProgress?: UploadInProgress;
+    fileStored?: FileStored;
 }
 
-const FileUpload: React.FC<Props> = ({ onUpload }: Props): JSX.Element => {
+const FileUploadContent = ({
+    status,
+    open,
+    error,
+    uploadInProgress = {},
+    fileStored = {},
+}: FileUploadContentProps): JSX.Element => {
     const { t } = useTranslation();
+    switch (status) {
+        case 'ERROR':
+            return (
+                <Fragment>
+                    <span className="typography__body file-upload__description">
+                        <span className="file-upload__error-prefix">{t('ui:file-upload.error-pre-content')}</span>
+                        {t('ui:file-upload.error-message.' + error)} Please{' '}
+                        <button onClick={open} className="typography__body--button-link">
+                            {t('ui:file-upload.error-upload')}
+                        </button>
+                        .
+                    </span>
+                    <span className="typography__small typography__small--secondary file-upload__extra">
+                        {t('ui:file-upload.error-extra.' + error)}
+                        {error === 'server' ? (
+                            <Link to="/contact-us" className="typography__body--link">
+                                {t('ui:file-upload.error-extra.contact-us') + '.'}
+                            </Link>
+                        ) : null}
+                    </span>
+                </Fragment>
+            );
+        case 'UPLOADING':
+            return (
+                <Fragment>
+                    <span className="typography__body file-upload__description">
+                        {t('ui:file-upload.uploading-message')}
+                        <span className="file-upload__progress-percentage">{uploadInProgress.progress}%</span>
+                    </span>
+                    <span className="typography__small typography__small--secondary file-upload__extra">
+                        {uploadInProgress.fileName}
+                    </span>
+                </Fragment>
+            );
+        case 'COMPLETE':
+            return (
+                <Fragment>
+                    <span className="typography__body file-upload__description">
+                        {t('ui:file-upload.complete-prefix')}
+                        <a className="typography__body--link" href={fileStored.previewLink}>
+                            {t('ui:file-upload.complete-preview')}
+                        </a>
+                        {t('ui:file-upload.complete-message')}
+                        <button onClick={open} className="typography__body--button-link">
+                            {t('ui:file-upload.complete-replace')}
+                        </button>
+                        {t('ui:file-upload.complete-suffix')}
+                    </span>
+                    <span className="typography__small typography__small--secondary file-upload__extra">
+                        {fileStored.fileName}
+                    </span>
+                </Fragment>
+            );
+        default:
+            return (
+                <Fragment>
+                    <span className="typography__body file-upload__description">
+                        <button onClick={open} className="typography__body--button-link">
+                            {t('ui:file-upload.idle-upload')}
+                        </button>
+                        {t('ui:file-upload.idle-message')}
+                    </span>{' '}
+                    <span className="typography__small typography__small--secondary file-upload__extra">
+                        {t('ui:file-upload.idle-extra')}
+                    </span>
+                </Fragment>
+            );
+    }
+};
+
+interface Props {
+    onUpload<T extends File>(acceptedFiles: T[], rejectedFiles: T[], event: DropEvent): void;
+    state?: {
+        uploadInProgress?: UploadInProgress;
+        error?: UploadErrors;
+        fileStored?: FileStored;
+    };
+}
+
+const FileUpload: React.FC<Props> = ({ onUpload, state = {} }: Props): JSX.Element => {
     const onDrop = useCallback(onUpload, []);
-    const { getRootProps, getInputProps } = useDropzone({ onDrop });
+    const { getRootProps, getInputProps, open, isDragActive } = useDropzone({ onDrop, noClick: true });
+
+    const status = useMemo(() => {
+        if (state.error && status !== 'ERROR') {
+            return 'ERROR';
+        }
+        if (state.uploadInProgress && status !== 'UPLOADING') {
+            return 'UPLOADING';
+        }
+        if (state.fileStored && status !== 'COMPLETE') {
+            return 'COMPLETE';
+        }
+        if (status !== 'IDLE') {
+            return 'IDLE';
+        }
+    }, [state]);
 
     return (
-        <div className="manuscript-upload">
-            <div className="manuscript-upload__dropzone" {...getRootProps()}>
+        <div className="file-upload">
+            <div
+                className={`file-upload__dropzone file-upload__dropzone--${status.toLowerCase()} ${
+                    isDragActive ? 'file-upload__dropzone--drag-active' : ''
+                }`}
+                {...getRootProps()}
+            >
                 <input {...getInputProps()} />
-                <UploadProgress />
-                <div className="manuscript-upload__content manuscript-upload__content--inactive">
-                    <p> {t('ui:manuscript-upload.inactive-content')}</p>
+                <UploadProgress progress={state.uploadInProgress && state.uploadInProgress.progress} status={status} />
+                <div className="file-upload__content">
+                    <FileUploadContent
+                        status={status}
+                        open={open}
+                        error={state.error}
+                        uploadInProgress={state.uploadInProgress}
+                        fileStored={state.fileStored}
+                    />
                 </div>
             </div>
         </div>
--- src/ui/stories/molecules/FileUpload.stories.tsx
+++ src/ui/stories/molecules/FileUpload.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, select, text, number, boolean } from '@storybook/addon-knobs';
+import { MemoryRouter } from 'react-router';
+import { action } from '@storybook/addon-actions';
 import { FileUpload } from '../../molecules';
 
 storiesOf('ui | molecules/FileUpload', module)
@@ -8,12 +10,44 @@ storiesOf('ui | molecules/FileUpload', module)
     .add(
         'FileUpload',
         (): JSX.Element => {
+            const uploadInProgress = boolean('Upload in progress?', false);
+            const fileStored = boolean('File already stored?', false);
+            const fileName = text('File Name', 'SomeFile.pdf');
+            const previewLink = text('File prview link', 'https://elifesciences.org/');
+            const progress = number('Upload progress', 0, { range: true, min: 0, max: 100 });
+            const error = select(
+                'Error State',
+                {
+                    'No Error': null,
+                    'Multiple files': 'multiple',
+                    'File size': 'size',
+                    'Server error': 'server',
+                },
+                null,
+            );
             return (
-                <FileUpload
-                    onUpload={files => {
-                        console.log(files);
-                    }}
-                />
+                <MemoryRouter initialEntries={['/']}>
+                    <div style={{ maxWidth: '700px', width: '100vw', padding: '20px' }}>
+                        <FileUpload
+                            state={{
+                                uploadInProgress: uploadInProgress
+                                    ? {
+                                          progress,
+                                          fileName,
+                                      }
+                                    : null,
+                                error,
+                                fileStored: fileStored
+                                    ? {
+                                          fileName,
+                                          previewLink,
+                                      }
+                                    : null,
+                            }}
+                            onUpload={action('File Uploaded')}
+                        />
+                    </div>
+                </MemoryRouter>
             );
         },
     );
