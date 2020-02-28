0bd0af160add8c1195b8fee0e01cee3db10ffce8
Will Byrne <w.byrne@elifesciences.org>
2020-02-28 14:00:07 +0000

    add files step (#135)
    * add file step page
    
    * rename file and add render test
    
    * remove unused import

---
+++ src/initial-submission/stories/FileDetailsForm.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered/react';
+import FileDetailsForm from '../components/FileDetailsForm';
+import '../../core/styles/index.scss';
+
+storiesOf('Initial Submission | steps/FileDetails', module)
+    .addDecorator(centered)
+    .add(
+        'FileDetails',
+        (): JSX.Element => {
+            return <FileDetailsForm />;
+        },
+    );
