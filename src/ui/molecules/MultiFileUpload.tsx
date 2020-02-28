c3174a91fe3f440f143f9eeb363f426d344afec9
Cory <c.donkin@elifesciences.org>
2020-02-28 15:38:08 +0000

    grid layout of supporting files list

--- src/ui/molecules/MultiFileUpload.tsx
+++ src/ui/molecules/MultiFileUpload.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import UploadProgress from './UploadProgress';
+import Delete from '@material-ui/icons/Delete';
+
 type File = {
     fileName: string;
 };
@@ -11,6 +14,21 @@ const MultiFileUpload = ({ files = [] }: Props): JSX.Element => {
     const { t } = useTranslation();
     return (
         <div className="multifile-upload">
+            <div className="multifile-upload__upload-list">
+                {files.map(file => {
+                    return (
+                        <div key={file.fileName} className="multifile-upload__upload-list-item">
+                            <div className="multifile-upload__progress">
+                                <UploadProgress status="COMPLETE" />
+                            </div>
+                            <div>{file.fileName}</div>
+                            <div>
+                                <Delete className="multifile-upload__delete" />
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
             <label
                 id="add-computer-button"
                 htmlFor="multiFileUpload"
--- src/ui/stories/molecules/MultiFileUpload.stories.tsx
+++ src/ui/stories/molecules/MultiFileUpload.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, object } from '@storybook/addon-knobs';
 import { MultiFileUpload } from '../../molecules';
 
 storiesOf('ui | molecules/MultiFileUpload', module)
@@ -8,6 +8,13 @@ storiesOf('ui | molecules/MultiFileUpload', module)
     .add(
         'MultiFileUpload',
         (): JSX.Element => {
-            return <MultiFileUpload />;
+            const files = object('Files', [
+                { fileName: 'File 1.pdf' },
+                { fileName: 'File 2.pdf' },
+                { fileName: 'File 3.pdf' },
+                { fileName: 'File 4.pdf' },
+                { fileName: 'File 5.pdf' },
+            ]);
+            return <MultiFileUpload files={files} />;
         },
     );
