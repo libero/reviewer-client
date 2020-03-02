c81ed2831c3fe366c343e4bad7ded406ccdf31bc
Cory <c.donkin@elifesciences.org>
2020-03-02 14:03:03 +0000

    start adding status messages to MultiFileUpload

--- src/ui/molecules/MultiFileUpload.tsx
+++ src/ui/molecules/MultiFileUpload.tsx
@@ -1,32 +1,74 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import UploadProgress from './UploadProgress';
 import Delete from '@material-ui/icons/Delete';
 
-type File = {
-    fileName: string;
+type UploadErrors = 'multiple' | 'size' | 'server';
+type UploadInProgress = {
+    progress?: number;
+    fileName?: string;
+};
+type FileStored = {
+    fileName?: string;
+    previewLink?: string;
+};
+
+type FileState = {
+    uploadInProgress?: UploadInProgress;
+    error?: UploadErrors;
+    fileStored?: FileStored;
 };
 
 interface Props {
-    files?: File[];
+    files?: FileState[];
 }
+
+const FileItem = ({ uploadInProgress, error, fileStored }: FileState) => {
+    const status = useMemo(() => {
+        if (error && status !== 'ERROR') {
+            return 'ERROR';
+        }
+        if (uploadInProgress && status !== 'UPLOADING') {
+            return 'UPLOADING';
+        }
+        if (fileStored && status !== 'COMPLETE') {
+            return 'COMPLETE';
+        }
+        if (status !== 'IDLE') {
+            return 'IDLE';
+        }
+    }, [uploadInProgress, error, fileStored]);
+
+    return (
+        <div className="multifile-upload__upload-list-item">
+            <div className="multifile-upload__progress">
+                <UploadProgress status={status} progress={uploadInProgress && uploadInProgress.progress} small />
+            </div>
+            <span className={`multfile-upload__file-name--${status.toLowerCase()}`}>
+                {status === 'COMPLETE' ? fileStored.fileName : uploadInProgress.fileName}
+                {status !== 'COMPLETE' ? (
+                    <span
+                        className={`multfile-upload__file-status typography__small typography__small--secondary multfile-upload__file-status--${status.toLowerCase()}`}
+                    >
+                        {' '}
+                        - {status === 'UPLOADING' ? `uploading ${uploadInProgress.progress}%` : 'Error.'}
+                    </span>
+                ) : null}
+            </span>
+            <div>
+                <Delete className="multifile-upload__delete" />
+            </div>
+        </div>
+    );
+};
+
 const MultiFileUpload = ({ files = [] }: Props): JSX.Element => {
     const { t } = useTranslation();
     return (
         <div className="multifile-upload">
             <div className="multifile-upload__upload-list">
-                {files.map(file => {
-                    return (
-                        <div key={file.fileName} className="multifile-upload__upload-list-item">
-                            <div className="multifile-upload__progress">
-                                <UploadProgress status="COMPLETE" small />
-                            </div>
-                            <div>{file.fileName}</div>
-                            <div>
-                                <Delete className="multifile-upload__delete" />
-                            </div>
-                        </div>
-                    );
+                {files.map((file, index) => {
+                    return <FileItem key={index} {...file} />;
                 })}
             </div>
             <label
--- src/ui/stories/molecules/MultiFileUpload.stories.tsx
+++ src/ui/stories/molecules/MultiFileUpload.stories.tsx
@@ -3,18 +3,33 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, object } from '@storybook/addon-knobs';
 import { MultiFileUpload } from '../../molecules';
 
+type UploadErrors = 'multiple' | 'size' | 'server';
+type UploadInProgress = {
+    progress?: number;
+    fileName?: string;
+};
+type FileStored = {
+    fileName?: string;
+    previewLink?: string;
+};
+
+type File = {
+    uploadInProgress?: UploadInProgress;
+    error?: UploadErrors;
+    fileStored?: FileStored;
+};
+
 storiesOf('ui | molecules/MultiFileUpload', module)
     .addDecorator(withKnobs)
     .add(
         'MultiFileUpload',
         (): JSX.Element => {
             const files = object('Files', [
-                { fileName: 'File 1.pdf' },
-                { fileName: 'File 2.pdf' },
-                { fileName: 'File 3.pdf' },
-                { fileName: 'File 4.pdf' },
-                { fileName: 'File 5.pdf' },
-            ]);
+                { fileStored: { fileName: 'File 1.pdf' } },
+                { uploadInProgress: { fileName: 'File 2.pdf', progress: 42 } },
+                { uploadInProgress: { fileName: 'File 3.pdf' }, error: 'multiple' },
+                { fileStored: { fileName: 'File 4.pdf' } },
+            ] as File[]);
             return <MultiFileUpload files={files} />;
         },
     );
