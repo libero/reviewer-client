e0857ef0dff677f3af67d74ae0a778445512a010
norisalsaadie <norisalsaadie@Noriss-MacBook-Pro.local>
2020-03-06 14:15:51 +0000

    Redirect if no valid token

--- src/core/utils/createApolloClient.ts
+++ src/core/utils/createApolloClient.ts
@@ -1,5 +1,5 @@
 import ApolloClient, { InMemoryCache } from 'apollo-boost';
-import { getToken } from '../../login/utils/tokenUtils';
+import { getToken, clearToken } from '../../login/utils/tokenUtils';
 
 interface Headers {
     authorization: string;
@@ -23,6 +23,22 @@ export default (host: string): ApolloClient<unknown> => {
         },
         request: async (operation): Promise<void> => {
             await operation.setContext(getHeaders(getToken()));
+        },
+        // TODO: figure out generic handler for errors
+        onError: ({ graphQLErrors = [], networkError }): void => {
+            graphQLErrors.forEach(({ message, locations, path }) =>
+                console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
+            );
+            if (networkError) console.log(`[Network error]: ${networkError}`);
+
+            const authenticationError = graphQLErrors.some(
+                error => error.extensions.code && error.extensions.code == 'UNAUTHENTICATED',
+            );
+
+            if (authenticationError) {
+                clearToken();
+                window.location.reload();
+            }
         },
     });
 };
