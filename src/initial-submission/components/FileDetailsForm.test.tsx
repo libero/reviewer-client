86788e38df5059f8a3e4482bc42ea5b9c96afefd
Cory <c.donkin@elifesciences.org>
2020-03-09 13:11:00 +0000

    unit test for cover letter onchange mutation call

--- src/initial-submission/components/FileDetailsForm.test.tsx
+++ src/initial-submission/components/FileDetailsForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent, wait } from '@testing-library/react';
 import React from 'react';
 import FileDetailsForm from './FileDetailsForm';
 const mutationMock = jest.fn();
@@ -27,5 +27,18 @@ describe('File Details Form', (): void => {
         expect(async () => {
             render(<FileDetailsForm initialValues={{ id: 'test' }} />);
         }).not.toThrow();
+    });
+
+    it('should call the save mutation with correct variables when cover letter is changed', async (): Promise<void> => {
+        const { container } = render(<FileDetailsForm initialValues={{ id: 'test' }} />);
+        fireEvent.input(container.querySelector('.cover-letter__input'), {
+            target: { value: 'test cover letter input' },
+        });
+        expect(mutationMock).toBeCalledWith({
+            variables: {
+                id: 'test',
+                coverLetter: 'test cover letter input',
+            },
+        });
     });
 });
