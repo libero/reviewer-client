a8f48cd616ad26399ed262dded8fa96b619b7b4c
Erez Odier <erez.odier@gmail.com>
2020-03-26 15:04:10 +0000

    WIP

--- src/core/utils/createApolloClient.ts
+++ src/core/utils/createApolloClient.ts
@@ -1,44 +1,80 @@
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import ApolloClient from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { WebSocketLink } from 'apollo-link-ws';
+import { split, ApolloLink, Observable } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
 import { getToken, clearToken } from '../../login/utils/tokenUtils';
 
 interface Headers {
     authorization: string;
 }
-const getHeaders = (authenticationToken?: string): Record<string, Headers> => ({
-    headers: {
-        authorization: authenticationToken ? `Bearer ${authenticationToken}` : '',
-    },
-});
 
 export default (host: string): ApolloClient<unknown> => {
+    const apiLink = new HttpLink({
+        uri: `${host}/graphql`, // use https for secure endpoint,
+    });
+
+    const authLink = setContext((_, { headers }) => {
+        // get the authentication token from local storage if it exists
+        const token = localStorage.getItem('token');
+        // return the headers to the context so httpLink can read them
+        return {
+            headers: {
+                ...headers,
+                authorization: token ? `Bearer ${token}` : '',
+            },
+        };
+    });
+
+    // TODO: figure out generic handler for errors
+    // const onError = ({ graphQLErrors = [], networkError }): void => {
+    //     graphQLErrors.forEach(({ message, locations, path }) =>
+    //         console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
+    //     );
+    //     if (networkError) console.log(`[Network error]: ${networkError}`);
+
+    //     const authenticationError = graphQLErrors.some(
+    //         error => error.extensions.code && error.extensions.code == 'UNAUTHENTICATED',
+    //     );
+
+    //     if (authenticationError) {
+    //         clearToken();
+    //         window.location.reload();
+    //     }
+    // };
+
+    const httpLink = ApolloLink.from([apiLink, authLink]);
+
+    const wsLink = new WebSocketLink({
+        uri: `${host}/graphql`.replace('http', 'ws'),
+        options: {
+            reconnect: true,
+        },
+    });
+
+    // using the ability to split links, you can send data to each link
+    // depending on what kind of operation is being sent
+    const link = split(
+        // split based on operation type
+        ({ query }) => {
+            const definition = getMainDefinition(query);
+            return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+        },
+        wsLink,
+        httpLink,
+    );
+
     return new ApolloClient({
         cache: new InMemoryCache(),
-        uri: `${host}/graphql`,
+        link,
         resolvers: {
             Query: {
                 isAuthenticated(): boolean {
                     return getToken() !== null;
                 },
             },
-        },
-        request: async (operation): Promise<void> => {
-            await operation.setContext(getHeaders(getToken()));
-        },
-        // TODO: figure out generic handler for errors
-        onError: ({ graphQLErrors = [], networkError }): void => {
-            graphQLErrors.forEach(({ message, locations, path }) =>
-                console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
-            );
-            if (networkError) console.log(`[Network error]: ${networkError}`);
-
-            const authenticationError = graphQLErrors.some(
-                error => error.extensions.code && error.extensions.code == 'UNAUTHENTICATED',
-            );
-
-            if (authenticationError) {
-                clearToken();
-                window.location.reload();
-            }
         },
     });
 };
