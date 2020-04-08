36ab116db0f41abea4c4b363303b0840eeb4aeba
Cory <c.donkin@elifesciences.org>
2020-04-08 15:01:48 +0100

    remove modulename mapper for translations from jest config

--- jest.config.js
+++ jest.config.js
@@ -8,8 +8,6 @@ module.exports = {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             '<rootDir>/test-utils/fileTransform.js',
         '\\.(css|scss)$': '<rootDir>/test-utils/styleTransform.js',
-        // "i18next": "<rootDir>/mocks/reacti18nextMock.js"
-        
     },
     globals: {
         CONFIG: {
