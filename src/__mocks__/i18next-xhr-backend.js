f52e0b402a0760bc44b39efb1b7e0af890e6f32d
noris alsaadie <>
2020-04-08 11:38:49 +0100

    Use mocks

---
+++ src/__mocks__/i18next-xhr-backend.js
@@ -0,0 +1,31 @@
+module.exports = class I18NextXhrBackend {
+    constructor() { }
+    init() {
+
+    }
+    readMulti(
+        languages,
+        namespaces,
+        callback
+    ) {
+
+    }
+    read(
+        language,
+        namespace,
+        callback
+    ) {
+
+    }
+    loadUrl(url, callback) {
+
+    }
+    create(
+        languages,
+        namespace,
+        key,
+        fallbackValue
+    ) {
+
+    }
+}
---
+++ src/__mocks__/i18next.js
@@ -0,0 +1,7 @@
+const stub = {
+    use: () => stub,
+    init: () => stub,
+    InitOptions: () => stub,
+}
+
+module.exports = stub
