df8b9d7d103a2ea200a46d77aec47212115d6f32
noris alsaadie <>
2020-05-06 12:54:40 +0100

    add basic interface to interact with

--- tests/page-objects/AuthorDetailsPage.ts
+++ tests/page-objects/AuthorDetailsPage.ts
@@ -9,27 +9,38 @@ export class AuthorDetailsPage {
     private readonly emailInput: string = '.orcid-details__email';
     private readonly institutionInput: string = '.orcid-details__institution';
 
-
     private browser: NightwatchBrowser;
 
     constructor(browser: NightwatchBrowser) {
         this.browser = browser;
     }
 
-    public setEmail(string): void {
-
+    public setEmail(input: string): void {
+        this.browser
+            .waitForElementVisible(this.emailInput)
+            .assert.visible(this.emailInput)
+            .setValue(this.emailInput, input);
     }
 
-    public setFirstName(string): void {
-
+    public setFirstName(input: string): void {
+        this.browser
+            .waitForElementVisible(this.firstNameInput)
+            .assert.visible(this.firstNameInput)
+            .setValue(this.firstNameInput, input);
     }
 
-    public setLastName(string): void {
-
+    public setLastName(input: string): void {
+        this.browser
+            .waitForElementVisible(this.lastNameInput)
+            .assert.visible(this.lastNameInput)
+            .setValue(this.lastNameInput, input);
     }
 
-    public setInstitution(string): void {
-        
+    public setInstitution(input: string): void {
+        this.browser
+            .waitForElementVisible(this.institutionInput)
+            .assert.visible(this.institutionInput)
+            .setValue(this.institutionInput, input);
     }
 
     public next(): void {
