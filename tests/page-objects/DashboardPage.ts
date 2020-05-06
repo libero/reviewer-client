27eb67dc88152d50f03396662f1b7c57f2995aa1
Cory <c.donkin@elifesciences.org>
2020-05-06 14:32:15 +0100

    whitespace change

--- tests/page-objects/DashboardPage.ts
+++ tests/page-objects/DashboardPage.ts
@@ -15,7 +15,6 @@ export class DashboardPage {
     constructor(browser: NightwatchBrowser) {
         this.browser = browser;
     }
-
     public onPage(): DashboardPage {
         this.browser.waitForElementVisible(this.newSubmissionButton, 10000);
         return this;
