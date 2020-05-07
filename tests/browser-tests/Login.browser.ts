25afe534fd6595cf50b396f3ab156c811136c1cc
Will Byrne <w.byrne@elifesciences.org>
2020-05-07 16:28:52 +0100

    unskip login test

--- tests/browser-tests/Login.browser.ts
+++ tests/browser-tests/Login.browser.ts
@@ -2,7 +2,7 @@ import { LoginPage, DashboardPage } from '../page-objects';
 
 fixture`Getting Started`.page`http://localhost:9000`;
 
-test.skip('My first test', async () => {
+test('My first test', async () => {
     const loginPage = new LoginPage();
     await loginPage.assertOnPage();
     await loginPage.login();
