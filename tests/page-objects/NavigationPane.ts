0cc7f1c14a6e78264b6e93b4b77e82b25610538c
noris alsaadie <>
2020-05-12 17:24:05 +0100

    Log out button

--- tests/page-objects/NavigationPane.ts
+++ tests/page-objects/NavigationPane.ts
@@ -1,13 +1,12 @@
 import { Selector, t } from 'testcafe';
 
 export class NavigationPane {
-
     private readonly iconLink = Selector('.app-bar__icon-link');
     private readonly burgerMenuContainer = Selector('.burger_menu');
     private readonly burgerMenuButton = Selector('.burger_menu__icon_button');
-    private readonly burgerMenuContent = Selector('.burger_menu__overlay')
+    private readonly burgerMenuContent = Selector('.burger_menu__overlay');
     private readonly menu = Selector('.burger_menu__list');
-    private readonly profileDropdown = Selector('.profile_dropdown')
+    private readonly profileDropdown = Selector('.profile_dropdown');
 
     public async assertOnPage(): Promise<void> {
         await t.expect(this.iconLink.visible).ok();
@@ -38,16 +37,28 @@ export class NavigationPane {
     }
 
     public async assertProfileDropDown(): Promise<void> {
-        const profileSelector = this.profileDropdown.child('profile_dropdown__panel');
+        const profileSelector = this.profileDropdown.child('.profile_dropdown__panel');
         await t.expect(this.profileDropdown.visible).ok();
         await t.expect(profileSelector.visible).notOk();
         await t.click(this.profileDropdown.child('button'));
         await t.expect(profileSelector.visible).ok();
-        await t.expect(profileSelector.child('profile_dropdown__panel_heading').visible).ok();
-        await t.expect(profileSelector.child('profile_dropdown__list').child().count).eql(2);
-        await t.expect(profileSelector.child('profile_dropdown__list').child(0).textContent).eql('Manage ORCID');
-        await t.expect(profileSelector.child('profile_dropdown__list').child(1).textContent).eql('Logout');
-        
+        await t.expect(profileSelector.child('.profile_dropdown__panel_heading').visible).ok();
+        await t.expect(profileSelector.child('.profile_dropdown__list').child().count).eql(2);
+        await t.expect(profileSelector.child('.profile_dropdown__list').child(0).textContent).eql('Manage ORCID');
+        await t.expect(profileSelector.child('.profile_dropdown__list').child(1).textContent).eql('Logout');
     }
 
+    public async logout(): Promise<void> {
+        const profileSelector = this.profileDropdown.child('.profile_dropdown__panel');
+        await t.expect(this.profileDropdown.visible).ok();
+        await t.expect(profileSelector.visible).notOk();
+        await t.click(this.profileDropdown.child('button'));
+        await t.expect(profileSelector.visible).ok();
+        await t.click(
+            profileSelector
+                .child('.profile_dropdown__list')
+                .child('.profile_dropdown__list_item')
+                .child('.profile_dropdown__link'),
+        );
+    }
 }
