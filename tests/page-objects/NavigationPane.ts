1e4800fefe395ead42f58162ce5fb69086b9ae77
noris alsaadie <>
2020-05-12 16:58:23 +0100

    WIP assert nav items

--- tests/page-objects/NavigationPane.ts
+++ tests/page-objects/NavigationPane.ts
@@ -3,22 +3,37 @@ import { Selector, t } from 'testcafe';
 export class NavigationPane {
 
     private readonly iconLink = Selector('.app-bar__icon-link');
-    private readonly burgerMenu = Selector('.burger_menu');
+    private readonly burgerMenuContainer = Selector('.burger_menu');
     private readonly burgerMenuButton = Selector('.burger_menu__icon_button');
     private readonly burgerMenuContent = Selector('.burger_menu__overlay')
+    private readonly menu = Selector('.burger_menu__list');
     private readonly profileDropdown = Selector('.profile_dropdown')
 
     public async assertOnPage(): Promise<void> {
         await t.expect(this.iconLink.visible).ok();
-        await t.expect(this.burgerMenu.visible).notOk();
+        await t.expect(this.burgerMenuContainer.visible).notOk();
         await t.expect(this.burgerMenuContent.visible).notOk();
+        await t.expect(this.menu.visible).notOk();
+        await t.expect(this.burgerMenuButton.visible).notOk();
         await t.expect(this.profileDropdown.visible).notOk();
     }
 
     public async assertOnPageAuthenticated(): Promise<void> {
         await t.expect(this.iconLink.visible).ok();
-        await t.expect(this.burgerMenu.visible).ok();
-        await t.expect(this.burgerMenuContent.visible).ok();
+        await t.expect(this.menu.visible).ok();
         await t.expect(this.profileDropdown.visible).ok();
+        // should only appear in responsive mode
+        await t.expect(this.burgerMenuContainer.visible).notOk();
+        await t.expect(this.burgerMenuContent.visible).notOk();
+        await t.expect(this.burgerMenuButton.visible).notOk();
     }
+
+    public async assertNavItems(): Promise<void> {
+        await t.expect(this.menu.child().count).eql(4);
+        await t.expect(this.menu.child(0).innerText).eql('Dashboard');
+        await t.expect(this.menu.child(0).innerText).eql('Author Guide');
+        await t.expect(this.menu.child(0).innerText).eql('Reviewer Guide');
+        await t.expect(this.menu.child(0).innerText).eql('Contact Us');
+    }
+
 }
