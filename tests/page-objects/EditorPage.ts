64322c7bba316784c72f1f630f7db311cfcc0096
noris alsaadie <>
2020-05-18 15:25:24 +0100

    Add search functionality

--- tests/page-objects/EditorPage.ts
+++ tests/page-objects/EditorPage.ts
@@ -18,9 +18,9 @@ export class EditorPage {
         await t.expect(this.seniorEditorsPicker.find('.selected_people_list__item').count).eql(1);
         const addButton = this.seniorEditorsPicker.find('.pod__button');
         await t.click(addButton);
+        await this.assertEditorSearch();
         await t.expect(Selector('.typography__heading--h2').visible).ok();
         await t.expect(Selector('.people-picker__selector_container').find('.typography__heading--h2').textContent).eql('Suggest Senior Editors');
-        await t.expect(Selector('#peoplePickerSearch').visible).ok();
         const button = Selector('.people-picker__modal_list--item').find('.pod__button');
         await t.expect(button.visible).ok();
         await t.click(button);
@@ -29,10 +29,22 @@ export class EditorPage {
         await t.expect(this.seniorEditorsPicker.find('.selected_people_list__item').count).eql(2);
     }
 
+    public async assertEditorSearch(): Promise<void> {
+        const searchInput = Selector('#peoplePickerSearch');
+        const peopleList  = Selector('.people-picker__modal_list');
+        await t.expect(searchInput.visible).ok();
+        await t.expect(peopleList.child('.people-picker__modal_list--item').count).gt(0);
+        await t.typeText(searchInput, 'should not exist');
+        await t.expect(peopleList.child('.people-picker__modal_list--item').count).eql(0);
+        await t.selectText(searchInput).pressKey('delete');
+        await t.expect(peopleList.child('.people-picker__modal_list--item').count).gt(0);
+    }
+
     public async addReviewer(): Promise<void> {
         await t.expect(this.suggestedReviewingEditorsPicker.find('.selected_people_list__item').count).eql(1);
         const addButton = this.suggestedReviewingEditorsPicker.find('.pod__button');
         await t.click(addButton);
+        await this.assertReviewerSearch();
         await t.expect(Selector('.typography__heading--h2').visible).ok();
         await t.expect(Selector('.people-picker__selector_container').find('.typography__heading--h2').textContent).eql('Suggest Reviewing Editors');
         await t.expect(Selector('#peoplePickerSearch').visible).ok();
@@ -42,6 +54,17 @@ export class EditorPage {
         await t.expect(Selector('.people-picker__selected-tabs').child('.people-picker__selected-tab').count).eql(1);
         await t.click(Selector('.modal__buttons_container').find('.button--primary'));
         await t.expect(this.suggestedReviewingEditorsPicker.find('.selected_people_list__item').count).eql(2);
+    }
+
+    public async assertReviewerSearch(): Promise<void> {
+        const searchInput = Selector('#peoplePickerSearch');
+        const peopleList  = Selector('.people-picker__modal_list');
+        await t.expect(searchInput.visible).ok();
+        await t.expect(peopleList.child('.people-picker__modal_list--item').count).gt(0);
+        await t.typeText(searchInput, 'should not exist');
+        await t.expect(peopleList.child('.people-picker__modal_list--item').count).eql(0);
+        await t.selectText(searchInput).pressKey('delete');
+        await t.expect(peopleList.child('.people-picker__modal_list--item').count).gt(0);
     }
 
     public async next(): Promise<void> {
