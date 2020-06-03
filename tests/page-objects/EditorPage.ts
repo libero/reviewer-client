d1cd828a6beae4ac24ed1628e78fce1fc687e4cf
noris alsaadie <>
2020-06-03 13:50:01 +0100

    Fix tests

--- tests/page-objects/EditorPage.ts
+++ tests/page-objects/EditorPage.ts
@@ -37,6 +37,25 @@ export class EditorPage {
         await this.addOpposingReviewer();
     }
 
+    private async addEditorsToPeoplePicker(picker: Selector): Promise<void> {
+        await t.expect(picker.find('.selected_people_list__item').count).eql(1);
+        const addButton = picker.find('.pod__button');
+        await t.click(addButton);
+        await this.assertPeoplePickerSearch();
+        await t.expect(Selector('.typography__heading--h2').visible).ok();
+        await t.expect(Selector('#peoplePickerSearch').visible).ok();
+        const firstAddButton = Selector('.people-picker__modal_list--item:nth-child(odd) .pod__button');
+        await t.expect(firstAddButton.visible).ok();
+        await t.click(firstAddButton);
+        const secondAddButton = Selector('.people-picker__modal_list--item:nth-child(even) .pod__button');
+        await t.expect(secondAddButton.visible).ok();
+        await t.click(secondAddButton);
+        await t.expect(Selector('.people-picker__selected-tabs').child('.people-picker__selected-tab').count).eql(2);
+        await t.click(Selector('.modal__buttons_container').find('.button--primary'));
+        await t.expect(Selector('.modal .modal__fullscreen').exists).eql(false);
+        await t.expect(picker.find('.selected_people_list__item').count).eql(3);
+    }
+
     private async addPersonToPeoplePicker(picker: Selector): Promise<void> {
         await t.expect(picker.find('.selected_people_list__item').count).eql(1);
         const addButton = picker.find('.pod__button');
@@ -44,20 +63,17 @@ export class EditorPage {
         await this.assertPeoplePickerSearch();
         await t.expect(Selector('.typography__heading--h2').visible).ok();
         await t.expect(Selector('#peoplePickerSearch').visible).ok();
-        const firstAddButton = Selector('.people-picker__modal_list--item:nth-child(odd)').find('.pod__button');
+        const firstAddButton = Selector('.people-picker__modal_list--item:nth-child(odd) .pod__button');
         await t.expect(firstAddButton.visible).ok();
         await t.click(firstAddButton);
-        const secondAddButton = Selector('.people-picker__modal_list--item:nth-child(even)').find('.pod__button');
-        await t.expect(secondAddButton.visible).ok();
-        await t.click(secondAddButton);
         await t.expect(Selector('.people-picker__selected-tabs').child('.people-picker__selected-tab').count).eql(1);
         await t.click(Selector('.modal__buttons_container').find('.button--primary'));
         await t.expect(Selector('.modal .modal__fullscreen').exists).eql(false);
-        await t.expect(picker.find('.selected_people_list__item').count).eql(3);
+        await t.expect(picker.find('.selected_people_list__item').count).eql(2);
     }
 
     public async addEditor(): Promise<void> {
-        await this.addPersonToPeoplePicker(this.seniorEditorsPicker);
+        await this.addEditorsToPeoplePicker(this.seniorEditorsPicker);
     }
 
     public async assertPeoplePickerSearch(): Promise<void> {
@@ -72,7 +88,7 @@ export class EditorPage {
     }
 
     public async addReviewer(): Promise<void> {
-        await this.addPersonToPeoplePicker(this.suggestedReviewingEditorsPicker);
+        await this.addEditorsToPeoplePicker(this.suggestedReviewingEditorsPicker);
     }
 
     public async addOpposingReviewingEditor(): Promise<void> {
