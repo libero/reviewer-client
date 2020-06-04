dba3a5f535e39c3034c3ae0526bac0d44c07eba3
noris alsaadie <>
2020-06-04 11:29:28 +0100

    more static content

--- src/static-pages/components/EditorialProcess.tsx
+++ src/static-pages/components/EditorialProcess.tsx
@@ -13,19 +13,21 @@ const ContactUsElife = (): JSX.Element => {
             <Paragraph type="writing">{t('editorial.editorial.p2')}</Paragraph>
 
             <div className="blue-box">
-                <h1>{t('editorial.editorial.pre-print.heading')}</h1>
+                <h2>{t('editorial.editorial.pre-print.heading')}</h2>
                 <Paragraph type="writing">{t('editorial.editorial.pre-print.p2')}</Paragraph>
                 <Paragraph type="writing">{t('editorial.editorial.pre-print.p2')}</Paragraph>
             </div>
+
             <Paragraph type="writing">{t('editorial.editorial.p3')}</Paragraph>
             <Paragraph type="writing">{t('editorial.editorial.p4')}</Paragraph>
 
             <div className="blue-box">
                 <Paragraph type="writing">{t('editorial.editorial.p5')}</Paragraph>
             </div>
+
             <Paragraph type="writing">{t('editorial.editorial.p6')}</Paragraph>
 
-            <h1>{t('editorial.initial-sub.heading')}</h1>
+            <h2>{t('editorial.initial-sub.heading')}</h2>
             <Paragraph type="writing">{t('editorial.initial-sub.p1')}</Paragraph>
             <ul>
                 <li>{t('editorial.initial-sub.bullet1')}</li>
@@ -33,11 +35,41 @@ const ContactUsElife = (): JSX.Element => {
                 <li>{t('editorial.initial-sub.bullet3')}</li>
             </ul>
 
-            <h1>{t('editorial.full-sub.heading')}</h1>
+            <h2>{t('editorial.full-sub.heading')}</h2>
             <Paragraph type="writing">{t('editorial.full-sub.p1')}</Paragraph>
 
-            <h1>{t('editorial.revised-sub.heading')}</h1>
+            <h2>{t('editorial.revised-sub.heading')}</h2>
             <Paragraph type="writing">{t('editorial.revised-sub.p1')}</Paragraph>
+
+            <h2>{t('editorial.faqs.heading')}</h2>
+
+            <h3>{t('editorial.faqs.question1')}</h3>
+            <Paragraph type="writing">{t('editorial.faqs.answer1')}</Paragraph>
+
+            <h3>{t('editorial.faqs.question2')}</h3>
+            <Paragraph type="writing">{t('editorial.faqs.answer2')}</Paragraph>
+
+            <h3>{t('editorial.faqs.question3')}</h3>
+            <Paragraph type="writing">{t('editorial.faqs.answer3')}</Paragraph>
+
+            <h3>{t('editorial.faqs.question4')}</h3>
+            <Paragraph type="writing">{t('editorial.faqs.answer4')}</Paragraph>
+
+            <h3>{t('editorial.faqs.question5')}</h3>
+            <Paragraph type="writing">{t('editorial.faqs.answer5')}</Paragraph>
+
+            <h3>{t('editorial.faqs.question6')}</h3>
+            <Paragraph type="writing">{t('editorial.faqs.answer6')}</Paragraph>
+
+            <h3>{t('editorial.faqs.question7')}</h3>
+            <Paragraph type="writing">{t('editorial.faqs.answer7')}</Paragraph>
+
+            <h3>{t('editorial.faqs.question8')}</h3>
+            <Paragraph type="writing">{t('editorial.faqs.answer8')}</Paragraph>
+
+            <div className="blue-box">
+                <Paragraph type="writing">{t('editorial.faqs.p1')}</Paragraph>
+            </div>
         </div>
     );
 };
