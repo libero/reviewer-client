8be385d5836f24b7695ef614cb8590ec119f90e2
noris alsaadie <>
2020-06-08 11:56:39 +0100

    WIP

--- src/static-pages/components/JournalPolicies.tsx
+++ src/static-pages/components/JournalPolicies.tsx
@@ -19,6 +19,177 @@ const JournalPolicies = (): JSX.Element => {
             <Paragraph type="writing">
                 <Interwave content={t('journal-policies.p3')} />
             </Paragraph>
+
+            <h2>{t('journal-policies.misconduct.heading')}</h2>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.misconduct.p1')} />
+            </Paragraph>
+
+            <h2>{t('journal-policies.experiments.heading')}</h2>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.experiments.p1')} />
+            </Paragraph>
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.experiments.p2')} />
+            </Paragraph>
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.experiments.p3')} />
+            </Paragraph>
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.experiments.p4')} />
+            </Paragraph>
+
+            <h2>{t('journal-policies.authorship.heading')}</h2>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.authorship.p1')} />
+            </Paragraph>
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.authorship.p2')} />
+            </Paragraph>
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.authorship.p3')} />
+            </Paragraph>
+
+            <h2>{t('journal-policies.cell-lines.heading')}</h2>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.cell-lines.p1')} />
+            </Paragraph>
+
+            <h2>{t('journal-policies.competing-interests.heading')}</h2>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.competing-interests.p1')} />
+            </Paragraph>
+
+            <h2>{t('journal-policies.compliance.heading')}</h2>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.compliance.p1')} />
+            </Paragraph>
+
+            <ul>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.compliance.bullet1')} />
+                    </Paragraph>
+                </li>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.compliance.bullet2')} />
+                    </Paragraph>
+                </li>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.compliance.bullet3')} />
+                    </Paragraph>
+                </li>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.compliance.bullet4')} />
+                    </Paragraph>
+                </li>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.compliance.bullet5')} />
+                    </Paragraph>
+                </li>
+            </ul>
+
+            <h2>{t('journal-policies.copywrite.heading')}</h2>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.copywrite.p1')} />
+            </Paragraph>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.competing-interests.p1')} />
+            </Paragraph>
+
+            <h2>{t('journal-policies.open-data.heading')}</h2>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.open-data.p1')} />
+            </Paragraph>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.open-data.p2')} />
+            </Paragraph>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.open-data.p3')} />
+            </Paragraph>
+
+            <ul>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.open-data.bullet1')} />
+                    </Paragraph>
+                </li>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.open-data.bullet2')} />
+                    </Paragraph>
+                </li>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.open-data.bullet3')} />
+                    </Paragraph>
+                </li>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.open-data.bullet4')} />
+                    </Paragraph>
+                </li>
+            </ul>
+
+            <h3>{t('journal-policies.open-data.data.heading')}</h3>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.open-data.data.p1')} />
+            </Paragraph>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.open-data.data.p2')} />
+            </Paragraph>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.open-data.data.p3')} />
+            </Paragraph>
+
+            <ul>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.open-data.data.bullet1')} />
+                    </Paragraph>
+                </li>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.open-data.data.bullet2')} />
+                    </Paragraph>
+                </li>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.open-data.data.bullet3')} />
+                    </Paragraph>
+                </li>
+                <li>
+                    <Paragraph type="writing">
+                        <Interwave content={t('journal-policies.open-data.data.bullet4')} />
+                    </Paragraph>
+                </li>
+            </ul>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.open-data.data.p4')} />
+            </Paragraph>
+
+            <Paragraph type="writing">
+                <Interwave content={t('journal-policies.open-data.data.p5')} />
+            </Paragraph>
         </div>
     );
 };
