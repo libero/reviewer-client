63789cdf3f77481236a1f59c801d08e7c7d8c8b3
noris alsaadie <>
2020-06-10 14:44:59 +0100

    Fix jumping layout issue + lack of tbody

--- src/static-pages/components/RequiredInfoTable.tsx
+++ src/static-pages/components/RequiredInfoTable.tsx
@@ -7,202 +7,204 @@ const RequiredInfoTable = (): JSX.Element => {
     return (
         <div className="table-guide">
             <table>
-                <tr>
-                    <th />
-                    <th>{t('initial-submission.manuscript.table.cols.journal')}</th>
-                    <th>{t('initial-submission.manuscript.table.cols.book')}</th>
-                    <th>{t('initial-submission.manuscript.table.cols.website')}</th>
-                    <th>{t('initial-submission.manuscript.table.cols.data-citation')}</th>
-                    <th>{t('initial-submission.manuscript.table.cols.software')}</th>
-                    <th>{t('initial-submission.manuscript.table.cols.pre-print')}</th>
-                    <th>{t('initial-submission.manuscript.table.cols.conference-proceedings')}</th>
-                    <th>{t('initial-submission.manuscript.table.cols.periodical')}</th>
-                    <th>{t('initial-submission.manuscript.table.cols.technical-report')}</th>
-                    <th>{t('initial-submission.manuscript.table.cols.thesis')}</th>
-                    <th>{t('initial-submission.manuscript.table.cols.patent')}</th>
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.authors')}</th>
-                    <td>✓</td>
-                    <td>✓*</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓°</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.editors')}</th>
-                    <td />
-                    <td>✓†</td>
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.year')}</th>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td />
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓§</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.pub-date')}</th>
-                    <td />
-                    <td />
-                    <td>✓</td>
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                    <td>✓</td>
-                    <td />
-                    <td />
-                    <td />
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.title')}</th>
-                    <td>✓</td>
-                    <td>✓*</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓*</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td />
-                    <td />
-                    <td>✓</td>
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.pub-title')}</th>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓*</td>
-                    <td>✓</td>
-                    <td>✓*</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                    <td />
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.vol')}</th>
-                    <td>✓*</td>
-                    <td>✓*</td>
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                    <td>✓*</td>
-                    <td />
-                    <td />
-                    <td />
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.page-range')}</th>
-                    <td>✓</td>
-                    <td>✓†</td>
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                    <td>✓*</td>
-                    <td>✓</td>
-                    <td />
-                    <td />
-                    <td />
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.id')}</th>
-                    <td>✓*</td>
-                    <td>✓*</td>
-                    <td />
-                    <td>✓*</td>
-                    <td>✓*</td>
-                    <td>✓*</td>
-                    <td>✓*</td>
-                    <td />
-                    <td>✓*</td>
-                    <td>✓*</td>
-                    <td />
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.location')}</th>
-                    <td />
-                    <td>✓*</td>
-                    <td />
-                    <td />
-                    <td>✓*</td>
-                    <td />
-                    <td>✓*</td>
-                    <td />
-                    <td>✓*</td>
-                    <td>✓</td>
-                    <td>✓</td>
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.publisher')}</th>
-                    <td />
-                    <td>✓</td>
-                    <td />
-                    <td />
-                    <td>✓*</td>
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                    <td>✓*</td>
-                    <td />
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.edition')}</th>
-                    <td />
-                    <td>✓*</td>
-                    <td />
-                    <td />
-                    <td />
-                    <td />
-                    <td>✓*</td>
-                    <td />
-                    <td>✓*</td>
-                    <td />
-                    <td />
-                </tr>
-                <tr>
-                    <th>{t('initial-submission.manuscript.table.rows.edition')}</th>
-                    <td />
-                    <td />
-                    <td>✓</td>
-                    <td>✓*</td>
-                    <td>✓*</td>
-                    <td>✓*</td>
-                    <td>✓</td>
-                    <td />
-                    <td>✓*</td>
-                    <td>✓*</td>
-                    <td>✓</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <th />
+                        <th>{t('initial-submission.manuscript.table.cols.journal')}</th>
+                        <th>{t('initial-submission.manuscript.table.cols.book')}</th>
+                        <th>{t('initial-submission.manuscript.table.cols.website')}</th>
+                        <th>{t('initial-submission.manuscript.table.cols.data-citation')}</th>
+                        <th>{t('initial-submission.manuscript.table.cols.software')}</th>
+                        <th>{t('initial-submission.manuscript.table.cols.pre-print')}</th>
+                        <th>{t('initial-submission.manuscript.table.cols.conference-proceedings')}</th>
+                        <th>{t('initial-submission.manuscript.table.cols.periodical')}</th>
+                        <th>{t('initial-submission.manuscript.table.cols.technical-report')}</th>
+                        <th>{t('initial-submission.manuscript.table.cols.thesis')}</th>
+                        <th>{t('initial-submission.manuscript.table.cols.patent')}</th>
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.authors')}</th>
+                        <td>✓</td>
+                        <td>✓*</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓°</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.editors')}</th>
+                        <td />
+                        <td>✓†</td>
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.year')}</th>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td />
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓§</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.pub-date')}</th>
+                        <td />
+                        <td />
+                        <td>✓</td>
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td>✓</td>
+                        <td />
+                        <td />
+                        <td />
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.title')}</th>
+                        <td>✓</td>
+                        <td>✓*</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓*</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td />
+                        <td />
+                        <td>✓</td>
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.pub-title')}</th>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓*</td>
+                        <td>✓</td>
+                        <td>✓*</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                        <td />
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.vol')}</th>
+                        <td>✓*</td>
+                        <td>✓*</td>
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td>✓*</td>
+                        <td />
+                        <td />
+                        <td />
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.page-range')}</th>
+                        <td>✓</td>
+                        <td>✓†</td>
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td>✓*</td>
+                        <td>✓</td>
+                        <td />
+                        <td />
+                        <td />
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.id')}</th>
+                        <td>✓*</td>
+                        <td>✓*</td>
+                        <td />
+                        <td>✓*</td>
+                        <td>✓*</td>
+                        <td>✓*</td>
+                        <td>✓*</td>
+                        <td />
+                        <td>✓*</td>
+                        <td>✓*</td>
+                        <td />
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.location')}</th>
+                        <td />
+                        <td>✓*</td>
+                        <td />
+                        <td />
+                        <td>✓*</td>
+                        <td />
+                        <td>✓*</td>
+                        <td />
+                        <td>✓*</td>
+                        <td>✓</td>
+                        <td>✓</td>
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.publisher')}</th>
+                        <td />
+                        <td>✓</td>
+                        <td />
+                        <td />
+                        <td>✓*</td>
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td>✓*</td>
+                        <td />
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.edition')}</th>
+                        <td />
+                        <td>✓*</td>
+                        <td />
+                        <td />
+                        <td />
+                        <td />
+                        <td>✓*</td>
+                        <td />
+                        <td>✓*</td>
+                        <td />
+                        <td />
+                    </tr>
+                    <tr>
+                        <th>{t('initial-submission.manuscript.table.rows.edition')}</th>
+                        <td />
+                        <td />
+                        <td>✓</td>
+                        <td>✓*</td>
+                        <td>✓*</td>
+                        <td>✓*</td>
+                        <td>✓</td>
+                        <td />
+                        <td>✓*</td>
+                        <td>✓*</td>
+                        <td>✓</td>
+                    </tr>
+                </tbody>
             </table>
 
             <ul>
