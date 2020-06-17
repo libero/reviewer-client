3ba6e44a440981066c22e468a5b48402c6fe87de
Will Byrne <w.byrne@elifesciences.org>
2020-06-17 11:34:00 +0100

    fix linting

--- src/initial-submission/components/SubmissionWizard.tsx
+++ src/initial-submission/components/SubmissionWizard.tsx
@@ -1,5 +1,5 @@
 /*eslint-disable react/display-name*/
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useParams, Switch, Route, Redirect, RouteComponentProps } from 'react-router-dom';
 import { Button, Modal, Paragraph } from '../../ui/atoms';
 import { ProgressBar } from '../../ui/molecules';
