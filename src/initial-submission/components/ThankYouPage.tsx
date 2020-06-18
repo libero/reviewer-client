3b3307de88af912444d0a0068a44eb90ad1e5662
noris alsaadie <>
2020-06-18 15:20:50 +0100

    fix deepscan issues

--- src/initial-submission/components/ThankYouPage.tsx
+++ src/initial-submission/components/ThankYouPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Paragraph } from '../../ui/atoms';
 import { Link } from 'react-router-dom';
