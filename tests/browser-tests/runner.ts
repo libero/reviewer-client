2ec1a8ed030af3ba79002ba71b0593e4b8800000
noris alsaadie <>
2020-06-24 15:04:04 +0100

    get working with typescript

--- tests/browser-tests/runner.ts
+++ tests/browser-tests/runner.ts
@@ -1,21 +1,20 @@
 const createTestCafe = require('testcafe');
-// console.log('createTestCafe', createTestCafe);
 
 async function start() {
     const testCafe = await createTestCafe(null, 1337, 1338);
     console.log('__dirname', __dirname);
     const runner = testCafe.createRunner();
-    const remoteConnection = await testCafe.createBrowserConnection();
+    // const remoteConnection = await testCafe.createBrowserConnection();
 
-    // Visit this URL from the remote device.
-    console.log(remoteConnection.url);
+    // // Visit this URL from the remote device.
+    // console.log(remoteConnection.url);
 
-    // Wait until the remote device's browser connects.
-    await new Promise(resolve => remoteConnection.once('ready', resolve));
+    // // Wait until the remote device's browser connects.
+    // await new Promise(resolve => remoteConnection.once('ready', resolve));
 
     await runner
-        .browsers(remoteConnection)
-        .src('./**/*.ts')
+        .src('./tests/browser-tests/Example.browser.ts')
+        .browsers('chrome')
         .run();
 }
-start();
+start().then(() => process.exit(0));
