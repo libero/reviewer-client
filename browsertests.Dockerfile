aa81b7bd2275a28ef4ceabd8a6b9c285e031d951
Noris Alsaadie <norisalsaadie@users.noreply.github.com>
2020-06-25 17:28:29 +0100

    Try await for react render on browser tests

--- browsertests.Dockerfile
+++ browsertests.Dockerfile
@@ -2,6 +2,11 @@ FROM testcafe/testcafe
 
 WORKDIR /app
 
+USER root
+RUN npm install testcafe-react-selectors testcafe
+USER user
+
+
 COPY tests tests
 COPY test-utils test-utils
 
--- tests/browser-tests/Minimal.browser.ts
+++ tests/browser-tests/Minimal.browser.ts
@@ -1,10 +1,12 @@
+import { waitForReact } from 'testcafe-react-selectors';
 import { LoginPage } from '../page-objects';
 import { BASE_URL } from '../../test-utils/baseUrl';
 
-fixture`Minimal`.page`${BASE_URL}`;
+fixture`Minimal`.page`${BASE_URL}`.beforeEach(async () => {
+    await waitForReact();
+});
 
-test('landing page is rendered', async() => {
+test('landing page is rendered', async () => {
     const loginPage = new LoginPage();
     await loginPage.assertOnPage();
 });
-
