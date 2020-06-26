775e49c109b7d7e5613d9275cad9211346058765
noris alsaadie <>
2020-06-26 09:33:35 +0100

    Add test pr

--- tests/browser-tests/Login.browser.ts
+++ tests/browser-tests/Login.browser.ts
@@ -14,6 +14,7 @@ import { BASE_URL } from '../../test-utils/baseUrl';
 
 fixture`Getting Started`.page`${BASE_URL}`.beforeEach(async () => {
     await waitForReact();
+    console.log('some test');
 });
 
 test('assert nav bar', async () => {
