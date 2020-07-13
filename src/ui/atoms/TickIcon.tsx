1a4efc61c716746d2906d2da84eccc31c41c1fa9
noris alsaadie <>
2020-07-13 12:07:10 +0100

    copy xpub

--- src/ui/atoms/TickIcon.tsx
+++ src/ui/atoms/TickIcon.tsx
@@ -2,18 +2,10 @@ import React from 'react';
 import createSvgIcon from '@material-ui/icons/utils/createSvgIcon';
 
 export default createSvgIcon(
-    <svg width="18px" height="14px" viewBox="0 0 18 14" version="1.1">
-        <title>Shape</title>
-        <g id="symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <g id="inputs/people-picker/selected" transform="translate(-369.000000, -53.000000)" fill="#0288D1">
-                <g id="selected" transform="translate(360.000000, 42.000000)">
-                    <polygon
-                        id="Shape"
-                        fill="#0288D1"
-                        points="15 22.2 10.8 18 9.4 19.4 15 25 27 13 25.6 11.6"
-                    ></polygon>
-                </g>
-            </g>
+    <svg viewBox="0 0 36 36">
+        <g fill="none" fill-rule="evenodd">
+            <circle cx="18" cy="18" r="18" fill="#0288D1"></circle>
+            <path d="M15 22.2L10.8 18l-1.4 1.4L15 25l12-12-1.4-1.4z" fill="#FFF"></path>
         </g>
     </svg>,
     'TickIcon',
