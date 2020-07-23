5cfaaa7b22fa25958a4bb24f26fd43ad242cd66c
Will Byrne <w.byrne@elifesciences.org>
2020-07-23 12:39:57 +0100

    fix browser test

--- tests/page-objects/LoginPage.ts
+++ tests/page-objects/LoginPage.ts
@@ -2,12 +2,26 @@ import { Selector, t } from 'testcafe';
 
 export class LoginPage {
     private readonly loginButton: Selector = Selector('.button--orcid');
+    private readonly cookieBanner: Selector = Selector('.cookie-banner');
+    private readonly cookieBannerButton: Selector = Selector('.cookie-banner__button');
 
     public async assertOnPage(): Promise<void> {
         await t.expect(this.loginButton.visible).ok();
     }
 
+    public async isCookieBannerVisible(): Promise<boolean> {
+        return this.cookieBanner.visible;
+    }
+
+    public async dismissCookieBanner(): Promise<void> {
+        await t.click(this.cookieBannerButton);
+        await t.expect(this.cookieBannerButton.visible).notOk();
+    }
+
     public async login(): Promise<void> {
         await t.click(this.loginButton);
+        if (this.isCookieBannerVisible) {
+            this.dismissCookieBanner();
+        }
     }
 }
