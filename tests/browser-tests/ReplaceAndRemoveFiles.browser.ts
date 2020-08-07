589fef0de09b5a526d483580cdf65910c630ac3a
Cory <c.donkin@elifesciences.org>
2020-08-07 09:55:38 +0100

    replace manuscript and delete a supporting file

---
+++ tests/browser-tests/ReplaceAndRemoveFiles.browser.ts
@@ -0,0 +1,32 @@
+import { waitForReact } from 'testcafe-react-selectors';
+
+import { NavigationHelper, FilesPage } from '../page-objects';
+import { BASE_URL } from '../../test-utils/baseUrl';
+
+fixture`Replace and remove files`.page`${BASE_URL}`.beforeEach(async () => {
+    await waitForReact();
+});
+
+test('Replace manuscript', async () => {
+    const navigationHelper = new NavigationHelper();
+    const filesPage = new FilesPage();
+    await navigationHelper.navigateToFilesPage();
+    await filesPage.uploadManuscriptFile('../test-data/dummy-manuscript.docx');
+    await filesPage.assertManuscriptStored('dummy-manuscript.docx');
+    await filesPage.uploadManuscriptFile('../test-data/dummy-manuscript2.pdf');
+    await filesPage.assertManuscriptStored('dummy-manuscript2.pdf');
+});
+
+test('Remove supporting files', async () => {
+    const navigationHelper = new NavigationHelper();
+    const filesPage = new FilesPage();
+    await navigationHelper.navigateToFilesPage();
+    await filesPage.uploadSupportingFiles(['../test-data/dummy-manuscript.docx', '../test-data/supporting-file.docx']);
+    await filesPage.assertSupportingFilesCount(2);
+    await filesPage.assertSupportingFileExists('supporting-file.docx');
+    await filesPage.assertSupportingFileExists('dummy-manuscript.docx');
+    await filesPage.deleteSupportingFile(0);
+    await filesPage.assertSupportingFilesCount(1);
+    await filesPage.assertSupportingFileExists('supporting-file.docx');
+    await filesPage.assertSupportingFileExists('dummy-manuscript.docx', false);
+});
