a488e10c1284ab4a19b1a5e5d063111943e9b04a
Will Byrne <w.byrne@elifesciences.org>
2020-08-10 14:02:54 +0100

    add test for deleting a submission (#371)

---
+++ tests/browser-tests/Dashboard.browser.ts
@@ -0,0 +1,21 @@
+import { waitForReact } from 'testcafe-react-selectors';
+import { t } from 'testcafe';
+import { DashboardPage, NavigationHelper, NavigationPane } from '../page-objects';
+import { BASE_URL } from '../../test-utils/baseUrl';
+
+fixture`Dashboard`.page`${BASE_URL}`.beforeEach(async () => {
+    await waitForReact();
+});
+
+test('User can delete a submission', async () => {
+    const navigationHelper = new NavigationHelper();
+    const navigationPane = new NavigationPane();
+    const id = await navigationHelper.navigateToAuthorPage();
+    await navigationPane.navigateToDashboard();
+    const dashboardPage = new DashboardPage();
+    const submissions = await dashboardPage.getSubmissions();
+    await t.expect(submissions.some(sub => sub.id === id)).ok();
+    await dashboardPage.deleteSubmission(id);
+    const submissionsAfterDelete = await dashboardPage.getSubmissions();
+    await t.expect(submissionsAfterDelete.some(sub => sub.id === id)).notOk();
+});
