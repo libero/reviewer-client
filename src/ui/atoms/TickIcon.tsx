74e75216a9d1b9c90423d199d5755696ce160e5f
Will Byrne <w.byrne@elifesciences.org>
2020-08-17 10:56:11 +0100

    fix people picker ux

--- src/ui/atoms/TickIcon.tsx
+++ src/ui/atoms/TickIcon.tsx
@@ -4,7 +4,7 @@ import createSvgIcon from '@material-ui/icons/utils/createSvgIcon';
 export default createSvgIcon(
     <svg viewBox="0 0 36 36">
         <g fill="none" fillRule="evenodd">
-            <circle cx="18" cy="18" r="18" fill="#0288D1"></circle>
+            <circle cx="18" cy="18" r="18" fill="#087acc"></circle>
             <path d="M15 22.2L10.8 18l-1.4 1.4L15 25l12-12-1.4-1.4z" fill="#FFF"></path>
         </g>
     </svg>,
