f8d29608da6452e67d13271a21c8523b279d85c4
Will Byrne <w.byrne@elifesciences.org>
2020-08-18 15:03:22 +0100

    fix grids (#381)

--- src/dashboard/components/NoSubmissions.tsx
+++ src/dashboard/components/NoSubmissions.tsx
@@ -14,39 +14,39 @@ const NoSubmissions = ({ onStartClick }: Props): JSX.Element => {
         <div className="no-submissions">
             <TwoColumnLayout>
                 <ImageWithAttribution
+                    className="two-column-layout__left two-column-layout__full_column"
                     image={Image}
                     artistName={t('image-artist')}
                     artistUrl="http://www.davidebonazzi.com/"
                     align="right"
                 />
-                <div>
-                    <h1 className="typography__heading typography__heading--h1">{t('heading')}</h1>
 
-                    <div className="no-submissions__text">
-                        <Paragraph type="writing">{t('new-system-1')}</Paragraph>
-                        <Paragraph type="writing">
-                            {t('new-system-2')}
-                            <strong>{t('research-advance')}</strong>
-                            {t('new-system-3')}
-                            <a className="typography typography__body--link" href="https://submit.elifesciences.org">
-                                {t('legacy')}
-                            </a>
-                        </Paragraph>
-                        <Paragraph type="writing">
-                            {t('new-system-4')}
-                            <a
-                                className="typography typography__body--link"
-                                href="https://reviewer.elifesciences.org/contact-us/contact-elife"
-                            >
-                                {t('contact')}
-                            </a>
-                        </Paragraph>
-                    </div>
-                    <div className="no-submissions__buttons">
-                        <Button id="new-submission-button" type="primary" onClick={(): void => onStartClick()}>
-                            {t('new-submission')}
-                        </Button>
-                    </div>
+                <h1 className="typography__heading typography__heading--h1 two-column-layout__right">{t('heading')}</h1>
+
+                <div className="no-submissions__text two-column-layout__right">
+                    <Paragraph type="writing">{t('new-system-1')}</Paragraph>
+                    <Paragraph type="writing">
+                        {t('new-system-2')}
+                        <strong>{t('research-advance')}</strong>
+                        {t('new-system-3')}
+                        <a className="typography typography__body--link" href="https://submit.elifesciences.org">
+                            {t('legacy')}
+                        </a>
+                    </Paragraph>
+                    <Paragraph type="writing">
+                        {t('new-system-4')}
+                        <a
+                            className="typography typography__body--link"
+                            href="https://reviewer.elifesciences.org/contact-us/contact-elife"
+                        >
+                            {t('contact')}
+                        </a>
+                    </Paragraph>
+                </div>
+                <div className="no-submissions__buttons two-column-layout__right">
+                    <Button id="new-submission-button" type="primary" onClick={(): void => onStartClick()}>
+                        {t('new-submission')}
+                    </Button>
                 </div>
             </TwoColumnLayout>
         </div>
