5216cfea49a6cc029411ba9fdeb6707dab023e79
noris alsaadie <>
2020-08-28 16:28:29 +0100

    Fix tests

--- src/dashboard/components/Routes.tsx
+++ src/dashboard/components/Routes.tsx
@@ -1,12 +1,14 @@
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch } from 'react-router-dom';
 const Dashboard = lazy(() => import('./Dashboard'));
 const AuthRoute = lazy(() => import('../../core/components/AuthRoute'));
 
 const Routes: React.FC = (): JSX.Element => (
-    <Switch>
-        <AuthRoute exact path="/" component={Dashboard} />
-    </Switch>
+    <Suspense fallback={<div />}>
+        <Switch>
+            <AuthRoute exact path="/" component={Dashboard} />
+        </Switch>
+    </Suspense>
 );
 
 export default Routes;
