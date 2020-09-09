e7df725198655fd2e07446e0b439cb92c0a7bc42
Noris Alsaadie <norisalsaadie@users.noreply.github.com>
2020-09-09 09:14:29 +0100

    use test args instead
    More sensible and stable way to test

--- tests/browser-tests/Minimal.browser.ts
+++ tests/browser-tests/Minimal.browser.ts
@@ -1,11 +1,7 @@
-// import { waitForReact } from 'testcafe-react-selectors';
 import { LoginPage } from '../page-objects';
 import { BASE_URL } from '../../test-utils/baseUrl';
 
 fixture`Minimal`.page`${BASE_URL}`;
-// .beforeEach(async () => {
-//     await waitForReact();
-// });
 
 test('landing page is rendered', async () => {
     const loginPage = new LoginPage();
