5bd104743f2d81121d705873c84fa37bbfef1a9d
Will Byrne <w.byrne@elifesciences.org>
2020-09-14 14:56:40 +0100

    add await for journal page

--- tests/browser-tests/ExtendedAuthentication.browser.ts
+++ tests/browser-tests/ExtendedAuthentication.browser.ts
@@ -1,6 +1,6 @@
 import { DashboardPage, LoginPage, NavigationPane } from '../page-objects';
 import { BASE_URL } from '../../test-utils/baseUrl';
-import { t } from 'testcafe';
+import { t, Selector } from 'testcafe';
 
 fixture`ExtendedAuthentication`.page`${BASE_URL}`.meta('fixtureID', 'staging');
 
@@ -22,6 +22,7 @@ test('User can logout then login again', async () => {
     await navigationPane.assertOnPageAuthenticated();
     await navigationPane.logout();
     if (process.env.ORCID_LOGIN_REQUIRED) {
+        await t.expect(Selector('.wrapper.wrapper--site-header').exists).ok();
         await t.navigateTo(BASE_URL);
     }
     await loginPage.assertOnPage();
