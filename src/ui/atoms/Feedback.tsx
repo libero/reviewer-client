7bd4bc1d284ab77ee01691541d5486d7ded47d4a
noris alsaadie <>
2020-09-21 13:32:04 +0100

    WIP

--- src/ui/atoms/Feedback.tsx
+++ src/ui/atoms/Feedback.tsx
@@ -1,24 +1,25 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { useQuery, useMutation } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 import { useTranslation } from 'react-i18next';
 import Interweave from 'interweave';
 
-import { CLEAR_ERROR, APPLICATION_ERROR } from '../../initial-submission/graphql';
+import { APPLICATION_ERROR } from '../../initial-submission/graphql';
 
 const Feedback = (): JSX.Element => {
     const { t } = useTranslation('ui');
     const [isSticky, setSticky] = useState(false);
-    const [clearError] = useMutation(CLEAR_ERROR);
     const ref = useRef(null);
-    const handleScroll = (): void => {
-        if (ref.current) {
-            window.scrollY > ref.current.getBoundingClientRect().bottom ? setSticky(true) : setSticky(false);
-        }
-    };
 
-    //eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const clearErrorHandler = async (): Promise<void> => {
-        await clearError();
+    const { data } = useQuery(APPLICATION_ERROR);
+    const { feedback = {} } = data && data.feedback ? data : {};
+
+    const { message = null, error = null } = feedback;
+
+    const handleScroll = (): void => {
+        console.log('ref.current', ref.current);
+        if (ref.current && error) {
+            window.pageYOffset > ref.current.getBoundingClientRect().bottom ? setSticky(true) : setSticky(false);
+        }
     };
 
     useEffect(() => {
@@ -26,16 +27,7 @@ const Feedback = (): JSX.Element => {
         return (): void => {
             window.removeEventListener('scroll', () => handleScroll);
         };
-    }, []);
-
-    const { data } = useQuery(APPLICATION_ERROR);
-
-    if (!data || !data.feedback) {
-        return <React.Fragment />;
-    }
-
-    // dismissable: indicates if this feedback can be dismissed. Parked pending styling discussion.
-    const { message, error } = data.feedback;
+    }, [error]);
 
     return (
         <React.Fragment>
