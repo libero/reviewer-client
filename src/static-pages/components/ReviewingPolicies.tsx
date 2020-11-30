543349cbd217670228ca358ae1c29de808892da0
Cory <c.donkin@elifesciences.org>
2020-11-30 16:56:00 +0000

    linting fix

--- src/static-pages/components/ReviewingPolicies.tsx
+++ src/static-pages/components/ReviewingPolicies.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Interweave from 'interweave';
 import { useTranslation } from 'react-i18next';
-import { Paragraph } from '../../ui/atoms';
 
 const ReviewingPolicies = (): JSX.Element => {
     const { t } = useTranslation('reviewer-guide');
