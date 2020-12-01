23a5c224b2e4261bf5c747705b17f86548bbcd82
Cory <c.donkin@elifesciences.org>
2020-12-01 15:40:56 +0000

    update static pages content

--- src/static-pages/components/ContactUs.tsx
+++ src/static-pages/components/ContactUs.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Switch, useRouteMatch, useLocation, Route, Redirect } from 'react-router-dom';
 import PageNavigation from './PageNavigation';
 import ContactUsElife from './ContactUsElife';
-import EditorialStaff from './EditorialStaff';
-import ProductionStaff from './ProductionStaff';
 import { useTranslation } from 'react-i18next';
 
 const ContactUs = (): JSX.Element => {
@@ -16,23 +14,13 @@ const ContactUs = (): JSX.Element => {
     return (
         <React.Fragment>
             <PageNavigation
-                links={[
-                    { link: initialPath, label: t('links.contact-elife-link') },
-                    { link: `${path}/editorial-staff`, label: t('links.editors-link') },
-                    { link: `${path}/production-staff`, label: t('links.production-link') },
-                ]}
+                links={[{ link: initialPath, label: t('links.contact-elife-link') }]}
                 currentPath={currentPath}
             />
             <div className="static-page">
                 <Switch>
                     <Route path={`${path}/contact-elife`}>
                         <ContactUsElife />
-                    </Route>
-                    <Route path={`${path}/editorial-staff`}>
-                        <EditorialStaff />
-                    </Route>
-                    <Route path={`${path}/production-staff`}>
-                        <ProductionStaff />
                     </Route>
                     <Redirect to={initialPath} />
                 </Switch>
