982817efe37ed704c107aedd25ca30ddc565a679
Dave Price <d.price@elifesciences.org>
2021-06-15 08:26:34 +0100

    linting correction

--- src/ui/molecules/ToggleComponent.tsx
+++ src/ui/molecules/ToggleComponent.tsx
@@ -1,19 +1,38 @@
 import React, { ReactNode } from 'react';
-import { SelectField } from '../atoms';
+import { ContentToggle, SelectField } from '../atoms';
+
+export interface Value {
+    label: string;
+    value: string;
+}
 
 interface Props {
     id: string;
+    selectId: string;
     children?: ReactNode;
-    toggleLabel: string;
-    open?: boolean;
-    onToggle?: Function;
+    labelText: string;
+    openText: string;
+    collapsedText: string;
+    questionTitle: string;
+    defaultValue?: Value;
+    values?: Value[];
 }
 
-const ToggleComponent = (): JSX.Element => {
+const ToggleComponent = ({
+    id,
+    selectId,
+    collapsedText,
+    openText,
+    questionTitle,
+    labelText,
+    defaultValue,
+    values,
+}: Props): JSX.Element => {
     return (
-        <div className="toggle">
-            <span className="burger">Test</span>
-            <SelectField id="" values={[]} labelText=""></SelectField>
+        <div className="toggle-component" id={id}>
+            <h3>{questionTitle}</h3>
+            <SelectField id={selectId} labelText={labelText} defaultValue={defaultValue} values={values}></SelectField>
+            <ContentToggle id={id} collapsedText={collapsedText} openText={openText}></ContentToggle>
         </div>
     );
 };
--- src/ui/stories/molecules/ToggleComponent.stories.tsx
+++ src/ui/stories/molecules/ToggleComponent.stories.tsx
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import '../../../core/styles/index.scss';
 import centered from '@storybook/addon-centered/react';
-import { SelectField } from '../../atoms';
+import { ContentToggle, SelectField } from '../../atoms';
 
 storiesOf('ui | molecules/Toggle', module)
     .addDecorator(withKnobs)
@@ -11,10 +11,25 @@ storiesOf('ui | molecules/Toggle', module)
     .add(
         'ToggleComponent',
         (): JSX.Element => {
+            const collapsedText = '+ Add a second country of residence/affiliation';
+            const openText = '- Remove a second country of residence/affiliation';
+            const title = 'Your country of residence or professional affiliation';
+            const labelText = 'Secondary country of residence/affiliation';
+            const options = [
+                { label: 'Argentina', value: 'argentina' },
+                { label: 'Canada', value: 'canada' },
+                { label: 'Denmark', value: 'denmark' },
+                { label: 'France', value: 'france' },
+                { label: 'Hungary', value: 'hungary' },
+                { label: 'Peru', value: 'peru' },
+            ];
             return (
-                <div className="toggle">
-                    <span className="burger">Test</span>
-                    <SelectField id="" values={[]} labelText=""></SelectField>
+                <div className="toggle-component">
+                    <h3 className="typography__heading typography__heading--h3">{title}</h3>
+                    <SelectField id="selectField" labelText="" values={options}></SelectField>
+                    <ContentToggle id="ContentToggle" collapsedText={collapsedText} openText={openText}>
+                        <SelectField id="SelectFieldTwo" labelText={labelText} values={options}></SelectField>
+                    </ContentToggle>
                 </div>
             );
         },
