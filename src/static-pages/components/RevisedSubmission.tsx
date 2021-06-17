98e6f2673cf6fd2db6c730e5490427b91c3872e9
NuclearRedeye <j.summerfield@elifesciences.org>
2021-06-17 15:14:15 +0100

    chore: add anchor links to revised submissions section

--- src/static-pages/components/RevisedSubmission.tsx
+++ src/static-pages/components/RevisedSubmission.tsx
@@ -2,29 +2,43 @@ import React from 'react';
 import Interweave from 'interweave';
 import { useTranslation } from 'react-i18next';
 import { Paragraph } from '../../ui/atoms';
+import useScrollToAnchor from '../../ui/hooks/useScrollToAnchor';
 
 const RevisedSubmission = (): JSX.Element => {
     const { t } = useTranslation('author-guide');
 
+    useScrollToAnchor();
+
     return (
         <div className="static-page__content">
-            <h1 className="typography__heading typography__heading--h1">{t('revised-submission.heading')}</h1>
+            <h1 id={t('revised-submission.anchor')} className="typography__heading typography__heading--h1">
+                {t('revised-submission.heading')}
+            </h1>
 
-            <h2 className="typography__heading typography__heading--h2">
+            <h2
+                id={t('revised-submission.decision-response.anchor')}
+                className="typography__heading typography__heading--h2"
+            >
                 {t('revised-submission.decision-response.heading')}
             </h2>
             <Paragraph type="reading">
                 <Interweave content={t('revised-submission.decision-response.p1')} />
             </Paragraph>
 
-            <h2 className="typography__heading typography__heading--h2">
+            <h2
+                id={t('revised-submission.source-manuscript.anchor')}
+                className="typography__heading typography__heading--h2"
+            >
                 {t('revised-submission.source-manuscript.heading')}
             </h2>
             <Paragraph type="reading">
                 <Interweave content={t('revised-submission.source-manuscript.p1')} />
             </Paragraph>
 
-            <h3 className="typography__heading typography__heading--h3">
+            <h3
+                id={t('revised-submission.source-manuscript.article-text.anchor')}
+                className="typography__heading typography__heading--h3"
+            >
                 {t('revised-submission.source-manuscript.article-text.heading')}
             </h3>
             <Paragraph type="reading">
@@ -37,14 +51,20 @@ const RevisedSubmission = (): JSX.Element => {
                 <Interweave content={t('revised-submission.source-manuscript.article-text.p3')} />
             </Paragraph>
 
-            <h3 className="typography__heading typography__heading--h3">
+            <h3
+                id={t('revised-submission.source-manuscript.key-resources.anchor')}
+                className="typography__heading typography__heading--h3"
+            >
                 {t('revised-submission.source-manuscript.key-resources.heading')}
             </h3>
             <Paragraph type="reading">
                 <Interweave content={t('revised-submission.source-manuscript.key-resources.p1')} />
             </Paragraph>
 
-            <h3 className="typography__heading typography__heading--h3">
+            <h3
+                id={t('revised-submission.source-manuscript.figures.anchor')}
+                className="typography__heading typography__heading--h3"
+            >
                 {t('revised-submission.source-manuscript.figures.heading')}
             </h3>
             <Paragraph type="reading">
@@ -60,7 +80,10 @@ const RevisedSubmission = (): JSX.Element => {
                 <Interweave content={t('revised-submission.source-manuscript.figures.p4')} />
             </Paragraph>
 
-            <h2 className="typography__heading typography__heading--h2">
+            <h2
+                id={t('revised-submission.source-manuscript.impact-statement.anchor')}
+                className="typography__heading typography__heading--h2"
+            >
                 {t('revised-submission.source-manuscript.impact-statement.heading')}
             </h2>
 
@@ -68,7 +91,10 @@ const RevisedSubmission = (): JSX.Element => {
                 <Interweave content={t('revised-submission.source-manuscript.impact-statement.p1')} />
             </Paragraph>
 
-            <h2 className="typography__heading typography__heading--h2">
+            <h2
+                id={t('revised-submission.source-manuscript.funding-information.anchor')}
+                className="typography__heading typography__heading--h2"
+            >
                 {t('revised-submission.source-manuscript.funding-information.heading')}
             </h2>
 
@@ -80,14 +106,20 @@ const RevisedSubmission = (): JSX.Element => {
                 <Interweave content={t('revised-submission.source-manuscript.funding-information.p2')} />
             </Paragraph>
 
-            <h2 className="typography__heading typography__heading--h2">
+            <h2
+                id={t('revised-submission.source-manuscript.digest.anchor')}
+                className="typography__heading typography__heading--h2"
+            >
                 {t('revised-submission.source-manuscript.digest.heading')}
             </h2>
             <Paragraph type="reading">
                 <Interweave content={t('revised-submission.source-manuscript.digest.p1')} />
             </Paragraph>
 
-            <h2 className="typography__heading typography__heading--h2">
+            <h2
+                id={t('revised-submission.source-manuscript.striking.anchor')}
+                className="typography__heading typography__heading--h2"
+            >
                 {t('revised-submission.source-manuscript.striking.heading')}
             </h2>
             <Paragraph type="reading">
