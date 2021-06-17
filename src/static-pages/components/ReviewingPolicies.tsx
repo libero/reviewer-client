f02d1b95f1034aa5902f75614e2d99148b0a218a
NuclearRedeye <j.summerfield@elifesciences.org>
2021-06-17 17:38:42 +0100

    chore: add anchor links to reviewing policies

--- src/static-pages/components/ReviewingPolicies.tsx
+++ src/static-pages/components/ReviewingPolicies.tsx
@@ -1,31 +1,49 @@
 import React from 'react';
 import Interweave from 'interweave';
 import { useTranslation } from 'react-i18next';
+import useScrollToAnchor from '../../ui/hooks/useScrollToAnchor';
 
 const ReviewingPolicies = (): JSX.Element => {
     const { t } = useTranslation('reviewer-guide');
 
+    useScrollToAnchor();
+
     return (
         <div className="static-page__content">
-            <h1 className="typography__heading typography__heading--h1">{t('reviewing-policies.heading')}</h1>
+            <h1 id={t('reviewing-policies.anchor')} className="typography__heading typography__heading--h1">
+                {t('reviewing-policies.heading')}
+            </h1>
             <Interweave content={t('reviewing-policies.content')} />
-            <h2 className="typography__heading typography__heading--h2">
+
+            <h2
+                id={t('reviewing-policies.confidentiality.anchor')}
+                className="typography__heading typography__heading--h2"
+            >
                 {t('reviewing-policies.confidentiality.heading')}
             </h2>
             <Interweave content={t('reviewing-policies.confidentiality.content')} />
 
-            <h2 className="typography__heading typography__heading--h2">{t('reviewing-policies.anonymity.heading')}</h2>
+            <h2 id={t('reviewing-policies.anonymity.anchor')} className="typography__heading typography__heading--h2">
+                {t('reviewing-policies.anonymity.heading')}
+            </h2>
             <Interweave content={t('reviewing-policies.anonymity.content')} />
 
-            <h2 className="typography__heading typography__heading--h2">
+            <h2
+                id={t('reviewing-policies.competing-interests.anchor')}
+                className="typography__heading typography__heading--h2"
+            >
                 {t('reviewing-policies.competing-interests.heading')}
             </h2>
             <Interweave content={t('reviewing-policies.competing-interests.content')} />
-            <h2 className="typography__heading typography__heading--h2">
+
+            <h2 id={t('reviewing-policies.misconduct.anchor')} className="typography__heading typography__heading--h2">
                 {t('reviewing-policies.misconduct.heading')}
             </h2>
             <Interweave content={t('reviewing-policies.misconduct.content')} />
-            <h2 className="typography__heading typography__heading--h2">{t('reviewing-policies.research.heading')}</h2>
+
+            <h2 id={t('reviewing-policies.research.anchor')} className="typography__heading typography__heading--h2">
+                {t('reviewing-policies.research.heading')}
+            </h2>
             <Interweave content={t('reviewing-policies.research.content')} />
         </div>
     );
