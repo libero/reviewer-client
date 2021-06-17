6f068c23e3abb7c7e51d5ffef97b23c69a004560
NuclearRedeye <j.summerfield@elifesciences.org>
2021-06-17 17:48:27 +0100

    chore: add anchor links to writing reviews section

--- src/static-pages/components/WritingReview.tsx
+++ src/static-pages/components/WritingReview.tsx
@@ -1,33 +1,61 @@
 import React from 'react';
 import Interweave from 'interweave';
 import { useTranslation } from 'react-i18next';
+import useScrollToAnchor from '../../ui/hooks/useScrollToAnchor';
 
 const WritingReview = (): JSX.Element => {
     const { t } = useTranslation('reviewer-guide');
 
+    useScrollToAnchor();
+
     return (
         <div className="static-page__content">
-            <h1 className="typography__heading typography__heading--h1">{t('writing-review.heading')}</h1>
+            <h1 id={t('writing-review.anchor')} className="typography__heading typography__heading--h1">
+                {t('writing-review.heading')}
+            </h1>
             <Interweave content={t('writing-review.content')} />
-            <h2 className="typography__heading typography__heading--h2">{t('writing-review.examples.heading')}</h2>
+
+            <h2 id={t('writing-review.examples.anchor')} className="typography__heading typography__heading--h2">
+                {t('writing-review.examples.heading')}
+            </h2>
             <Interweave content={t('writing-review.examples.content')} />
-            <h2 className="typography__heading typography__heading--h2">
+
+            <h2
+                id={t('writing-review.examples-public-review.anchor')}
+                className="typography__heading typography__heading--h2"
+            >
                 {t('writing-review.examples-public-review.heading')}
             </h2>
             <Interweave content={t('writing-review.examples-public-review.content')} />
-            <h3 className="typography__heading typography__heading--h3">
+
+            <h3
+                id={t('writing-review.examples-public-review.summary1.anchor')}
+                className="typography__heading typography__heading--h3"
+            >
                 {t('writing-review.examples-public-review.summary1.heading')}
             </h3>
             <Interweave content={t('writing-review.examples-public-review.summary1.content')} />
-            <h3 className="typography__heading typography__heading--h3">
+
+            <h3
+                id={t('writing-review.examples-public-review.public-review1.anchor')}
+                className="typography__heading typography__heading--h3"
+            >
                 {t('writing-review.examples-public-review.public-review1.heading')}
             </h3>
             <Interweave content={t('writing-review.examples-public-review.public-review1.content')} />
-            <h3 className="typography__heading typography__heading--h3">
+
+            <h3
+                id={t('writing-review.examples-public-review.summary2.anchor')}
+                className="typography__heading typography__heading--h3"
+            >
                 {t('writing-review.examples-public-review.summary2.heading')}
             </h3>
             <Interweave content={t('writing-review.examples-public-review.summary2.content')} />
-            <h3 className="typography__heading typography__heading--h3">
+
+            <h3
+                id={t('writing-review.examples-public-review.public-review2.anchor')}
+                className="typography__heading typography__heading--h3"
+            >
                 {t('writing-review.examples-public-review.public-review2.heading')}
             </h3>
             <Interweave content={t('writing-review.examples-public-review.public-review2.content')} />
