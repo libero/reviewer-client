4ef840f42981de6015c72487aadc0e5096913976
NuclearRedeye <j.summerfield@elifesciences.org>
2021-06-17 15:22:00 +0100

    chore: add anchor links to post decision section

--- src/static-pages/components/PostDecision.tsx
+++ src/static-pages/components/PostDecision.tsx
@@ -2,29 +2,42 @@ import React from 'react';
 import Interweave from 'interweave';
 import { useTranslation } from 'react-i18next';
 import { Paragraph } from '../../ui/atoms';
+import useScrollToAnchor from '../../ui/hooks/useScrollToAnchor';
 
 const PostDecision = (): JSX.Element => {
     const { t } = useTranslation('author-guide');
 
+    useScrollToAnchor();
+
     return (
         <div className="static-page__content">
-            <h1 className="typography__heading typography__heading--h1">{t('post.heading')}</h1>
+            <h1 id={t('post.anchor')} className="typography__heading typography__heading--h1">
+                {t('post.heading')}
+            </h1>
 
-            <h2 className="typography__heading typography__heading--h2">{t('post.public-reviews.heading')}</h2>
+            <h2 id={t('post.public-reviews.anchor')} className="typography__heading typography__heading--h2">
+                {t('post.public-reviews.heading')}
+            </h2>
 
             <Paragraph type="reading">
                 <Interweave content={t('post.public-reviews.p1')} />
             </Paragraph>
 
-            <h2 className="typography__heading typography__heading--h2">{t('post.acceptance.heading')}</h2>
+            <h2 id={t('post.acceptance.anchor')} className="typography__heading typography__heading--h2">
+                {t('post.acceptance.heading')}
+            </h2>
 
             <Paragraph type="reading">
                 <Interweave content={t('post.acceptance.p1')} />
             </Paragraph>
-            <h3 className="typography__heading typography__heading--h3">{t('post.acceptance.executable.heading')}</h3>
+            <h3 id={t('post.acceptance.executable.anchor')} className="typography__heading typography__heading--h3">
+                {t('post.acceptance.executable.heading')}
+            </h3>
             <Interweave content={t('post.acceptance.executable.content')} />
 
-            <h3 className="typography__heading typography__heading--h3">{t('post.acceptance.protocols.heading')}</h3>
+            <h3 id={t('post.acceptance.protocols.anchor')} className="typography__heading typography__heading--h3">
+                {t('post.acceptance.protocols.heading')}
+            </h3>
 
             <Paragraph type="reading">
                 <Interweave content={t('post.acceptance.protocols.p1')} />
@@ -43,7 +56,9 @@ const PostDecision = (): JSX.Element => {
                 </li>
             </ul>
 
-            <h3 className="typography__heading typography__heading--h3">{t('post.publication-manuscript.heading')}</h3>
+            <h3 id={t('post.publication-manuscript.anchor')} className="typography__heading typography__heading--h3">
+                {t('post.publication-manuscript.heading')}
+            </h3>
 
             <Paragraph type="reading">
                 <Interweave content={t('post.publication-manuscript.p1')} />
@@ -61,7 +76,9 @@ const PostDecision = (): JSX.Element => {
                 <Interweave content={t('post.publication-manuscript.p4')} />
             </Paragraph>
 
-            <h3 className="typography__heading typography__heading--h3">{t('post.final-version.heading')}</h3>
+            <h3 id={t('post.final-version.anchor')} className="typography__heading typography__heading--h3">
+                {t('post.final-version.heading')}
+            </h3>
 
             <Paragraph type="reading">
                 <Interweave content={t('post.final-version.p1')} />
@@ -71,7 +88,9 @@ const PostDecision = (): JSX.Element => {
                 <Interweave content={t('post.final-version.p2')} />
             </Paragraph>
 
-            <h2 className="typography__heading typography__heading--h2">{t('post.corrections.heading')}</h2>
+            <h2 id={t('post.corrections.anchor')} className="typography__heading typography__heading--h2">
+                {t('post.corrections.heading')}
+            </h2>
 
             <Paragraph type="reading">
                 <Interweave content={t('post.corrections.p1')} />
@@ -89,9 +108,13 @@ const PostDecision = (): JSX.Element => {
                 <Interweave content={t('post.corrections.p4')} />
             </Paragraph>
 
-            <h2 className="typography__heading typography__heading--h2">{t('post.rejections.heading')}</h2>
+            <h2 id={t('post.rejections.anchor')} className="typography__heading typography__heading--h2">
+                {t('post.rejections.heading')}
+            </h2>
 
-            <h3 className="typography__heading typography__heading--h3">{t('post.rejections.peers.heading')}</h3>
+            <h3 id={t('post.rejections.peers.anchor')} className="typography__heading typography__heading--h3">
+                {t('post.rejections.peers.heading')}
+            </h3>
 
             <Paragraph type="reading">
                 <Interweave content={t('post.rejections.peers.p1')} />
@@ -101,7 +124,9 @@ const PostDecision = (): JSX.Element => {
                 <Interweave content={t('post.rejections.peers.p2')} />
             </Paragraph>
 
-            <h3 className="typography__heading typography__heading--h3">{t('post.rejections.appeals.heading')}</h3>
+            <h3 id={t('post.rejections.appeals.anchor')} className="typography__heading typography__heading--h3">
+                {t('post.rejections.appeals.heading')}
+            </h3>
 
             <Paragraph type="reading">
                 <Interweave content={t('post.rejections.appeals.p1')} />
