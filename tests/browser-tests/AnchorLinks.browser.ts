95d7563f9146fc799a563e052d7a23e7d7ec762e
NuclearRedeye <j.summerfield@elifesciences.org>
2021-06-18 10:16:41 +0100

    chore: add test for clicking anchor link

--- tests/browser-tests/AnchorLinks.browser.ts
+++ tests/browser-tests/AnchorLinks.browser.ts
@@ -11,9 +11,17 @@ test('No anchor, defaults to top of page', async () => {
     await t.expect(getWindowScollY()).eql(0);
 });
 
-test('With anchor, scrolls element into view', async () => {
+test('With anchor, scrolls the section into view', async () => {
     await t.navigateTo('/author-guide/journal-policies#name-change');
     await t.wait(500); // Wait, so the useScrollToAnchor hook has run.
     const elementOffsetTop = await Selector('#name-change').offsetTop;
     await t.expect(getWindowScollY()).eql(elementOffsetTop);
+});
+
+test('Clicking an anchor link scrolls the section into view', async () => {
+  await t.navigateTo('/author-guide/journal-policies');
+  await t.wait(500); // Wait, so the useScrollToAnchor hook has run.
+  await t.click('a[href=\'#author-identification\']');
+  const elementOffsetTop = await Selector('#author-identification').offsetTop;
+  await t.expect(getWindowScollY()).eql(elementOffsetTop);
 });
