fb44281c9d1ff6e398b4f89a630ce3a59c74b737
NuclearRedeye <j.summerfield@elifesciences.org>
2021-06-18 10:05:27 +0100

    chore: add comment to make clear why we wait

--- tests/browser-tests/AnchorLinks.browser.ts
+++ tests/browser-tests/AnchorLinks.browser.ts
@@ -7,12 +7,13 @@ const getWindowScollY = ClientFunction(() => window.scrollY);
 
 test('No anchor, defaults to top of page', async () => {
     await t.navigateTo('/author-guide/journal-policies');
+    await t.wait(500); // Wait, so the useScrollToAnchor hook has run.
     await t.expect(getWindowScollY()).eql(0);
 });
 
 test('With anchor, scrolls element into view', async () => {
     await t.navigateTo('/author-guide/journal-policies#name-change');
-    await t.wait(500);
+    await t.wait(500); // Wait, so the useScrollToAnchor hook has run.
     const elementOffsetTop = await Selector('#name-change').offsetTop;
     await t.expect(getWindowScollY()).eql(elementOffsetTop);
 });
