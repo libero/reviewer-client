3f58f059072cd331e2048449e69b7654f05ef9d4
NuclearRedeye <j.summerfield@elifesciences.org>
2021-06-29 16:58:48 +0100

    chore: add question 1 and 2 to form

--- src/survey/components/SurveyPart1.tsx
+++ src/survey/components/SurveyPart1.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams, useHistory } from 'react-router-dom';
-import { Paragraph, TextField, Button } from '../../ui/atoms';
+import { Paragraph, TextField, Button, RadioButton } from '../../ui/atoms';
 import Interweave from 'interweave';
 import { useForm } from 'react-hook-form';
 import { SurveyInput, SurveyResponse } from '../types';
@@ -13,8 +13,20 @@ interface Props {
     register: () => void;
 }
 
-const SurveyPart1 = (props: Props): JSX.Element => {
+const SurveyPart1 = ({register}: Props): JSX.Element => {
     const { t } = useTranslation('survey');
+
+    const q1Options = [
+        { label: 'First Author (includes joint-first author)', value: 'first-author' },
+        { label: 'Last author (includes joint-last author)', value: 'last-author' },
+        { label: 'Neither of the above', value: 'neither' },
+    ];
+
+    const q2Options = [ 
+        { "label": "Yes", "value": "yes" },
+        { "label": "No", "value": "no" },
+        { "label": "Prefer not to say", "value": "neither" }
+    ];
 
     return (
         <div className="survey">
@@ -27,6 +39,8 @@ const SurveyPart1 = (props: Props): JSX.Element => {
             <Paragraph type="writing">
                 <Interweave content={t('p3')} />
             </Paragraph>
+            <RadioButton labelText={t('q1.label')} name='q1' options={q1Options} register={register}></RadioButton>
+            <RadioButton labelText={t('q2.label')} name='q2' options={q2Options} register={register}></RadioButton>
         </div>
     );
 };
