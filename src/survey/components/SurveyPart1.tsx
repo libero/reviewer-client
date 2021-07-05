b2bc33b57b3312dbbfac8a5f21ba0f7f1305793e
NuclearRedeye <j.summerfield@elifesciences.org>
2021-07-05 15:22:21 +0100

    feat: basic working component

--- src/survey/components/SurveyPart1.tsx
+++ src/survey/components/SurveyPart1.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams, useHistory } from 'react-router-dom';
 import { Paragraph, TextField, Button, RadioButton } from '../../ui/atoms';
@@ -11,9 +11,10 @@ import { useMutation } from '@apollo/react-hooks';
 
 interface Props {
     register: () => void;
+    showQuestion3?: boolean;
 }
 
-const SurveyPart1 = ({register}: Props): JSX.Element => {
+const SurveyPart1 = ({ showQuestion3 = false, register }: Props): JSX.Element => {
     const { t } = useTranslation('survey');
 
     const q1Options = [
@@ -22,10 +23,10 @@ const SurveyPart1 = ({register}: Props): JSX.Element => {
         { label: 'Neither of the above', value: 'neither' },
     ];
 
-    const q2Options = [ 
-        { "label": "Yes", "value": "yes" },
-        { "label": "No", "value": "no" },
-        { "label": "Prefer not to say", "value": "neither" }
+    const q2Options = [
+        { label: 'Yes', value: 'yes' },
+        { label: 'No', value: 'no' },
+        { label: 'Prefer not to say', value: 'neither' },
     ];
 
     return (
@@ -39,8 +40,18 @@ const SurveyPart1 = ({register}: Props): JSX.Element => {
             <Paragraph type="writing">
                 <Interweave content={t('p3')} />
             </Paragraph>
-            <RadioButton labelText={t('q1.label')} name='q1' options={q1Options} register={register}></RadioButton>
-            <RadioButton labelText={t('q2.label')} name='q2' options={q2Options} register={register}></RadioButton>
+            <h3 className="typography__heading typography__heading--h3">{t('q1.label')}</h3>
+            <RadioButton id="answers[1].answer" name="q1" options={q1Options} register={register}></RadioButton>
+            <h3 className="typography__heading typography__heading--h3">{t('q2.label')}</h3>
+            <RadioButton id="answers[2].answer" name="q2" helperText={t('q2.helper')} options={q2Options} register={register}></RadioButton>
+            {showQuestion3 && <TextField
+                name="q3"
+                id="answers[3].answer"
+                labelText={t('q3.label')}
+                placeholder="Enter text"
+                helperText={t('q3.helper')}
+                register={register}
+            />}
         </div>
     );
 };
