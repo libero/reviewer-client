0fe6d4439c66c49da2b44af25dd17b1f8d1641f0
NuclearRedeye <j.summerfield@elifesciences.org>
2021-07-06 10:56:06 +0100

    stash: safe keeping

--- src/survey/components/Survey.tsx
+++ src/survey/components/Survey.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams, useHistory } from 'react-router-dom';
 import { Paragraph, TextField, Button } from '../../ui/atoms';
@@ -9,17 +9,22 @@ import { SurveyInput, SurveyResponse } from '../types';
 import { saveSurvey } from '../graphql';
 import { useMutation } from '@apollo/react-hooks';
 import SurveyPart1 from './SurveyPart1';
+import SurveyPart2 from './SurveyPart2';
 
 const Survey = (): JSX.Element => {
     const { register, watch, formState } = useForm<SurveyInput>();
     const [saveCallback] = useMutation<SurveyResponse>(saveSurvey);
     const { t } = useTranslation('survey');
     const { id } = useParams();
-    const answers = watch('answers');
+    const answers = watch();
     const history = useHistory();
 
+    const numberOfPages = 2;
+    const [currentPage, setCurrentPage] = useState(1);
+
     const onClick = (): void => {
-        if (formState.dirty) {
+        setCurrentPage(currentPage === 1 ? 2 : 1);
+        /*if (formState.dirty) {
             const response: SurveyInput = {
                 surveyId: 'ediSurvey',
                 answers: answers.map((answer, index) => {
@@ -36,12 +41,36 @@ const Survey = (): JSX.Element => {
             saveCallback({ variables: response });
         }
         history.push(`/thankyou/${id}`);
+        */
     };
+
+    let independentResearcherValue = watch('independentResearcher');
+    const [showIndependentResearcherYear, setshowIndependentResearcherYear] = useState(false);
+
+    useEffect(() => {
+        setshowIndependentResearcherYear(independentResearcherValue === 'yes');
+    }, [independentResearcherValue]);
+
+    let genderIdentityValue = watch('genderIdentity');
+    const [showGenderSelfDescribe, setShowGenderSelfDescribe] = useState(false);
+
+    useEffect(() => {
+        setShowGenderSelfDescribe(genderIdentityValue === 'self-describe');
+    }, [genderIdentityValue]);
 
     return (
         <div className="survey">
-            <h2 className="typography__heading typography__heading--h2">{t('title')}</h2>
-            <SurveyPart1 register={register}></SurveyPart1>
+            <h2 className="typography__heading typography__heading--h2">{`${t(
+                'title',
+            )} (${currentPage}/${numberOfPages})`}</h2>
+            {currentPage === 1 ? (
+                <SurveyPart1
+                    showIndependentResearcherYear={showIndependentResearcherYear}
+                    register={register}
+                ></SurveyPart1>
+            ) : (
+                <SurveyPart2 showGenderSelfDescribe={showGenderSelfDescribe} register={register}></SurveyPart2>
+            )}
             <Button type="primary" onClick={onClick}>
                 {formState.dirty ? t('navigation.done') : t('navigation.skip')}
             </Button>
