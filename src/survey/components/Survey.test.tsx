8df5613c7cbb2289ab0a732da50b3034674ef635
NuclearRedeye <j.summerfield@elifesciences.org>
2021-07-08 17:24:52 +0100

    wip: add test for data submit

--- src/survey/components/Survey.test.tsx
+++ src/survey/components/Survey.test.tsx
@@ -20,6 +20,7 @@ jest.mock('@apollo/react-hooks', () => ({
 describe('Survey', (): void => {
     afterEach(() => {
         cleanup();
+        mutationMock.mockReset();
     });
 
     it('should render correctly', async (): Promise<void> => {
@@ -48,5 +49,70 @@ describe('Survey', (): void => {
         fireEvent.click(getByText('navigation.back'));
         await waitFor(() => {});
         expect(container.querySelector('#survey-part-1')).not.toBeNull();
+    });
+
+    // FIXME: This test should not be skipped
+    it.skip('should submit valid data', async (): Promise<void> => {
+        const defaultValues = {
+            submittingAs: 'first-author',
+            independentResearcher: 'yes',
+            independentResearcherYear: '1990',
+            genderIdentity: 'self-describe',
+            genderSelfDescribe: 'popsicle',
+            countryOfResidence: { label: 'USA', value: 'US' },
+            secondCountryOfResidence: { label: 'USA', value: 'US' },
+            countryIndentifyAs: 'yes',
+            raceOrEthnicity: 'jedi',
+        };
+
+        const result = {
+            variables: {
+                surveyId: 'ediSurvey',
+                answers: [
+                    {
+                        answer: 'first-author',
+                        text: 'submittingAs.label',
+                        questionId: 'submittingAs',
+                    },
+                    {
+                        answer: 'yes',
+                        text: 'independentResearcher.label',
+                        questionId: 'independentResearcher',
+                    },
+                    {
+                        answer: '1990',
+                        text: 'independentResearcherYear.label',
+                        questionId: 'independentResearcherYear',
+                    },
+                ],
+                submissionId: 'someid',
+            },
+        };
+
+        // FIXME: The ID set here, is not making it through to onSubmit callback.
+        const { getByText, getByLabelText, container } = render(<Survey />, {
+            wrapper: routerWrapper(['/survey/someid']),
+        });
+
+        fireEvent.click(getByLabelText('submittingAs.options-0'));
+        await waitFor(() => {});
+
+        fireEvent.click(getByLabelText('independentResearcher.options-0'));
+        await waitFor(() => {});
+
+        // FIXME: This isn't actually populating the text in the control
+        //fireEvent.change(getByLabelText('independentResearcherYear.label'), {target: { value: '1990'}});
+        //await waitFor(() => {});
+
+        fireEvent.click(getByText('navigation.next'));
+        await waitFor(() => {});
+
+        expect(container.querySelector('#survey-part-2')).not.toBeNull();
+
+        // FIXME: This should be skip not done, the components aren't correctly asserting if they are dirty or not.
+        fireEvent.click(getByText('navigation.done'));
+        await waitFor(() => {});
+
+        expect(mutationMock).toBeCalledWith(result);
     });
 });
