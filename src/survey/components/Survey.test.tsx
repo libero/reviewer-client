ae642151f585ca5b136d3d191a7de4e404aba712
NuclearRedeye <j.summerfield@elifesciences.org>
2021-07-08 16:15:37 +0100

    feat: update survey component test

--- src/survey/components/Survey.test.tsx
+++ src/survey/components/Survey.test.tsx
@@ -1,6 +1,6 @@
 import '../../../test-utils/i18n-mock';
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, fireEvent, render, waitFor } from '@testing-library/react';
 import Survey from './Survey';
 import routerWrapper from '../../../test-utils/routerWrapper';
 
@@ -17,7 +17,7 @@ jest.mock('@apollo/react-hooks', () => ({
     },
 }));
 
-describe.skip('Survey', (): void => {
+describe('Survey', (): void => {
     afterEach(() => {
         cleanup();
     });
@@ -26,5 +26,27 @@ describe.skip('Survey', (): void => {
         expect(async () => {
             render(<Survey />, { wrapper: routerWrapper(['/survey/someid']) });
         }).not.toThrow();
+    });
+
+    it('should start on page 1', async (): Promise<void> => {
+        const { container } = render(<Survey />, { wrapper: routerWrapper(['/survey/someid']) });
+        expect(container.querySelector('#survey-part-1')).not.toBeNull();
+    });
+
+    it('should navigate to the next page', async (): Promise<void> => {
+        const { getByText, container } = render(<Survey />, { wrapper: routerWrapper(['/survey/someid']) });
+        fireEvent.click(getByText('navigation.skip'));
+        await waitFor(() => {});
+        expect(container.querySelector('#survey-part-2')).not.toBeNull();
+    });
+
+    it('should navigate back the previous page', async (): Promise<void> => {
+        const { getByText, container } = render(<Survey />, { wrapper: routerWrapper(['/survey/someid']) });
+        fireEvent.click(getByText('navigation.skip'));
+        await waitFor(() => {});
+        expect(container.querySelector('#survey-part-2')).not.toBeNull();
+        fireEvent.click(getByText('navigation.back'));
+        await waitFor(() => {});
+        expect(container.querySelector('#survey-part-1')).not.toBeNull();
     });
 });
