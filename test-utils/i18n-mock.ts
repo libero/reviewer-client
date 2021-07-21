eed855fe75aa964709e52001a341d8cf47a423bb
NuclearRedeye <j.summerfield@elifesciences.org>
2021-07-21 11:49:04 +0100

    fix: update mock to return real data

--- test-utils/i18n-mock.ts
+++ test-utils/i18n-mock.ts
@@ -1,20 +1,16 @@
+import data from '../public/locales/en/survey.json';
+
 export {}
 jest.mock('react-i18next', () => ({
     // this mock makes sure any components using the translate HoC receive the t function as a prop
     useTranslation: () => {
-        const stub = { 
-            t: (str: string, opts: {} = {}) => {
-                if (opts['returnObjects']) {
-                    const obj = {}
-                    for (let i = 0; i < 5; i++) {
-                        obj[`${str}-${i}`] = `${str}-${i}`;
-                    }
-                    return obj;
-                } else {
-                    return str;
-                }
+        const stub = { t: (str: string, opts: {} = {}) => {
+            if (opts['returnObjects']) {
+                return data[str.split('.')[0]][str.split('.')[1]];
             }
-        };
+            return str
+        }};
         return stub;
     },
 }));
+
