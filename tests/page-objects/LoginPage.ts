b8b9324bb9f7f2778f1ec8bf1a827e75f86b9975
Dave <d.price@elifesciences.org>
2021-08-25 09:59:39 +0100

    Adding Cookiebot script tags and removing old cookie bar (#517)
    * Adding Cookiebot script tags and removing old cookie bar
    
    * Removing remainder of the old cookie banner
    
    * Move cover letter below manuscripts and make text changes
    
    * Reverting as in wrong branch
    
    * Removing script tag for cookiebot in body as not needed
    
    * Remove cookieBannerBox as not needed

--- tests/page-objects/LoginPage.ts
+++ tests/page-objects/LoginPage.ts
@@ -3,8 +3,6 @@ import { clickSelector } from './formHelper';
 
 export class LoginPage {
     private readonly loginButton: Selector = Selector('.button--orcid');
-    private readonly cookieBanner: Selector = Selector('.cookie-banner');
-    private readonly cookieBannerButton: Selector = Selector('.cookie-banner__button');
     private readonly orcidPage: Selector = Selector('.orcid-wizard');
     private readonly orcidIDInput: Selector = Selector('[formcontrolname=username]');
     private readonly orcidPasswordInput: Selector = Selector('[formcontrolname=password]');
@@ -15,20 +13,8 @@ export class LoginPage {
         await t.expect(this.loginButton.visible).ok();
     }
 
-    public async isCookieBannerVisible(): Promise<boolean> {
-        return this.cookieBanner.visible;
-    }
-
-    public async dismissCookieBanner(): Promise<void> {
-        await clickSelector('.cookie-banner__button');
-        await t.expect(this.cookieBannerButton.visible).notOk();
-    }
-
     public async login(orcidLoginOptional = false): Promise<void> {
         await clickSelector('.button--orcid');
-        if (await this.isCookieBannerVisible()) {
-            await this.dismissCookieBanner();
-        }
         await t.expect(this.loginButton.exists).notOk();
         if (process.env.ORCID_LOGIN_REQUIRED && process.env.ORCID_LOGIN_NAME && process.env.ORCID_LOGIN_PASSWORD) {
             const orcidPageVisible = await this.orcidPage.exists;
