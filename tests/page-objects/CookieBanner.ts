3d2872ddeecc81c60bb7bcbafd22c242bf446c0c
NuclearRedeye <j.summerfield@elifesciences.org>
2021-10-08 10:06:46 +0100

    fix: skip cookie banner checks if consent cookie is present

--- tests/page-objects/CookieBanner.ts
+++ tests/page-objects/CookieBanner.ts
@@ -1,19 +1,27 @@
-import { Selector, t } from 'testcafe';
+import { ClientFunction, Selector, t } from 'testcafe';
 import { clickSelector } from './formHelper';
+
+const hasConsentedToCookies = ClientFunction(() => {
+    return document.cookie.split(';').find(cookie => cookie.trimStart().startsWith('CookieConsent=')) ? true : false;
+});
 
 export class CookieBanner {
     private readonly cookieBanner: Selector = Selector('#CybotCookiebotDialog');
     private readonly cookieBannerButton: Selector = Selector('#CybotCookiebotDialogBodyLevelButtonAccept');
 
     public async assertOnPage(): Promise<void> {
-        await t.expect(this.cookieBanner.exists).ok();
-        await t.expect(this.cookieBanner.visible).ok();
-        await t.expect(this.cookieBannerButton.exists).ok();
-        await t.expect(this.cookieBannerButton.visible).ok();
+        if (!hasConsentedToCookies()) {
+            await t.expect(this.cookieBanner.exists).ok();
+            await t.expect(this.cookieBanner.visible).ok();
+            await t.expect(this.cookieBannerButton.exists).ok();
+            await t.expect(this.cookieBannerButton.visible).ok();
+        }
     }
 
     public async acceptCookies(): Promise<void> {
-        await clickSelector('#CybotCookiebotDialogBodyLevelButtonAccept');
-        await t.expect(this.cookieBannerButton.visible).notOk();
+        if (!hasConsentedToCookies()) {
+            await clickSelector('#CybotCookiebotDialogBodyLevelButtonAccept');
+            await t.expect(this.cookieBannerButton.visible).notOk();
+        }
     }
 }
