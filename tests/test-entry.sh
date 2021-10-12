b639ccc5d48a4d79479d26f57c5af161303592dd
NuclearRedeye <j.summerfield@elifesciences.org>
2021-10-12 12:09:35 +0100

    fix: increase timeout for browser tests

--- tests/test-entry.sh
+++ tests/test-entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -xe
 
-TEST="testcafe --sf --assertion-timeout 5000 --skip-js-errors chromium:headless --no-sandbox --disable-dev-shm-usage"
+TEST="testcafe --sf --assertion-timeout 15000 --skip-js-errors chromium:headless --no-sandbox --disable-dev-shm-usage"
 TEST_ARGS=${TEST_ARGS:-all}
 
 echo "Starting Test Suite: ${TEST_ARGS}"
