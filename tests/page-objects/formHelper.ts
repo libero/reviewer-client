b204e4dbfecfd14c7c4591af2f48e46a00fcad04
NuclearRedeye <j.summerfield@elifesciences.org>
2021-10-12 23:16:55 +0100

    fix: remove clientfunction calls from the form helpers

--- tests/page-objects/formHelper.ts
+++ tests/page-objects/formHelper.ts
@@ -1,4 +1,4 @@
-import { ClientFunction } from 'testcafe';
+import { Selector, t } from 'testcafe';
 
 async function hasError(selector: Selector): Promise<string> {
     const errorElement = selector.find('.typography__label--error');
@@ -9,39 +9,19 @@ async function hasError(selector: Selector): Promise<string> {
 }
 
 async function clickNext(): Promise<void> {
-    await ClientFunction(() => {
-        (document.querySelector('.submission-wizard-next-button') as HTMLElement).click();
-    })();
+    await t.click('.submission-wizard-next-button');
 }
 
 async function clickBack(): Promise<void> {
-    await ClientFunction(() => {
-        (document.querySelector('.submission-wizard-back-buttonn') as HTMLElement).click();
-    })();
+    await t.click('.submission-wizard-back-button');
 }
 
 async function clickSelector(selector: string): Promise<void> {
-    await ClientFunction((s: string) => {
-        const element = document.querySelector(s) as HTMLElement;
-        if (element) {
-            element.click();
-        } else {
-            throw new Error('Element not present');
-        }
-    })(selector); // this needs to be here since Client function needs the selector passed in.
+    await t.click(selector);
 }
 
 async function clickWithSelectorAndText(selector: string, text: string): Promise<void> {
-    await ClientFunction((s: string, t: string) => {
-        const element = Array.prototype.slice.call(document.querySelectorAll(s)).filter(function(el) {
-            return el.textContent.includes(t);
-        })[0];
-        if (element) {
-            element.click();
-        } else {
-            throw new Error('Element not present');
-        }
-    })(selector, text); // this needs to be here since Client function needs the selector passed in.
+    await t.click(Selector(selector).withText(text));
 }
 
 export { hasError, clickNext, clickSelector, clickWithSelectorAndText, clickBack };
