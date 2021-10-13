12718985c9e3fb0113b2e602d75e8f9ad57695db
NuclearRedeye <j.summerfield@elifesciences.org>
2021-10-13 17:48:52 +0100

    fix: move mouse to element prior to click

--- tests/page-objects/formHelper.ts
+++ tests/page-objects/formHelper.ts
@@ -8,20 +8,26 @@ async function hasError(selector: Selector): Promise<string> {
     return '';
 }
 
+async function clickSelector(selector: string): Promise<void> {
+    const element = Selector(selector);
+    await t.expect(element.exists).ok();
+    await t.expect(element.visible).ok();
+    await t.hover(element).click(element);
+}
+
 async function clickNext(): Promise<void> {
-    await t.click('.submission-wizard-next-button');
+    await clickSelector('.submission-wizard-next-button');
 }
 
 async function clickBack(): Promise<void> {
-    await t.click('.submission-wizard-back-button');
-}
-
-async function clickSelector(selector: string): Promise<void> {
-    await t.click(selector);
+    await clickSelector('.submission-wizard-back-button');
 }
 
 async function clickWithSelectorAndText(selector: string, text: string): Promise<void> {
-    await t.click(Selector(selector).withText(text));
+    const element = Selector(selector).withText(text);
+    await t.expect(element.exists).ok();
+    await t.expect(element.visible).ok();
+    await t.hover(element).click(element);
 }
 
 export { hasError, clickNext, clickSelector, clickWithSelectorAndText, clickBack };
