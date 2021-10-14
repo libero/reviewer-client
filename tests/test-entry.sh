318e92183e1aff7ea4b481db8e1e6ce3b4dbe7fe
NuclearRedeye <j.summerfield@elifesciences.org>
2021-10-14 21:11:14 +0100

    chore: reinstate assertion timeout for browser test container

--- tests/test-entry.sh
+++ tests/test-entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -xe
 
-TEST="testcafe --sf -e -u --disable-http2 chromium:headless --no-sandbox --disable-dev-shm-usage"
+TEST="testcafe --sf -e -u --disable-http2 --assertion-timeout 15000 chromium:headless --no-sandbox --disable-dev-shm-usage"
 TEST_ARGS=${TEST_ARGS:-all}
 
 echo "Starting Test Suite: ${TEST_ARGS}"
