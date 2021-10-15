72df37e10089c29719b9b7455192e7c675ef8fc5
NuclearRedeye <j.summerfield@elifesciences.org>
2021-10-15 08:43:53 +0100

    fix: update browser tests to retry on failures

--- tests/test-entry.sh
+++ tests/test-entry.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -xe
 
-TEST="testcafe --sf -e -u --disable-http2 --assertion-timeout 15000 chromium:headless --no-sandbox --disable-dev-shm-usage"
+TEST="testcafe --sf -e -u --disable-http2 chromium:headless --no-sandbox --disable-dev-shm-usage"
 TEST_ARGS=${TEST_ARGS:-all}
 
 echo "Starting Test Suite: ${TEST_ARGS}"
 
 if [ ${TEST_ARGS} == "all" ]
 then
-  ${TEST} 'tests/**/*.browser.ts'
+  find ./tests -name *.browser.ts -type f | sort | xargs -I % sh -c "${TEST} % -q || exit 255"
 else
-  ${TEST} 'tests/**/*.browser.ts' -f ${TEST_ARGS}
+  ${TEST} 'tests/**/*.browser.ts' -f ${TEST_ARGS} -q
 fi
 
