d9321082607749eace1d2c2f71530a552c03ea7b
Joel Summerfield <52923935+NuclearRedeye@users.noreply.github.com>
2022-03-18 14:14:44 +0000

    fix: cookie banner is not dismissed in browser tests (#544)

--- test-utils/cookieBanner.ts
+++ test-utils/cookieBanner.ts
@@ -1,23 +1,10 @@
-import { ClientFunction, Selector, t } from 'testcafe';
+import { Selector, t } from 'testcafe';
 
 const cookieBanner: Selector = Selector('#CybotCookiebotDialog');
 const cookieBannerButton: Selector = Selector('#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll');
 
-const getCookies = ClientFunction(() => {
-    return document.cookie;
-});
-
-async function hasConsentedToCookies(): Promise<boolean> {
-    let retVal = false;
-    const cookies = await getCookies();
-    if (cookies.length > 0) {
-        retVal = cookies.split(';').find(cookie => cookie.trim().startsWith('CookieConsent=')) ? true : false;
-    }
-    return retVal;
-}
-
 export async function dismissCookieBanner(): Promise<void> {
-    if (await !hasConsentedToCookies()) {
+    if (await cookieBanner.exists) {
         await t.expect(cookieBanner.exists).ok();
         await t.expect(cookieBanner.visible).ok();
         await t.expect(cookieBannerButton.exists).ok();
