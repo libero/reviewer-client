2b3c29a28eb404ddb1b19b6edee8396ecd75029b
Dave Price <d.price@elifesciences.org>
2022-11-11 10:28:37 +0000

    Reviewer text changes Oct 22

--- src/static-pages/components/WritingReview.tsx
+++ src/static-pages/components/WritingReview.tsx
@@ -15,11 +15,6 @@ const WritingReview = (): JSX.Element => {
             </h1>
             <Interweave content={t('writing-review.content')} />
 
-            <h2 id={t('writing-review.examples.anchor')} className="typography__heading typography__heading--h2">
-                {t('writing-review.examples.heading')}
-            </h2>
-            <Interweave content={t('writing-review.examples.content')} />
-
             <h2
                 id={t('writing-review.examples-public-review.anchor')}
                 className="typography__heading typography__heading--h2"
@@ -29,28 +24,12 @@ const WritingReview = (): JSX.Element => {
             <Interweave content={t('writing-review.examples-public-review.content')} />
 
             <h3
-                id={t('writing-review.examples-public-review.summary1.anchor')}
-                className="typography__heading typography__heading--h3"
-            >
-                {t('writing-review.examples-public-review.summary1.heading')}
-            </h3>
-            <Interweave content={t('writing-review.examples-public-review.summary1.content')} />
-
-            <h3
                 id={t('writing-review.examples-public-review.public-review1.anchor')}
                 className="typography__heading typography__heading--h3"
             >
                 {t('writing-review.examples-public-review.public-review1.heading')}
             </h3>
             <Interweave content={t('writing-review.examples-public-review.public-review1.content')} />
-
-            <h3
-                id={t('writing-review.examples-public-review.summary2.anchor')}
-                className="typography__heading typography__heading--h3"
-            >
-                {t('writing-review.examples-public-review.summary2.heading')}
-            </h3>
-            <Interweave content={t('writing-review.examples-public-review.summary2.content')} />
 
             <h3
                 id={t('writing-review.examples-public-review.public-review2.anchor')}
