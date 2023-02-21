4c15eb068bce9ab3d4913f5bfc113ccae8d31fbb
Will Byrne <will.alex.byrne@gmail.com>
2023-02-21 15:09:26 +0000

    fix story which was using a removed prop

--- src/dashboard/stories/NoSubmissions.stories.tsx
+++ src/dashboard/stories/NoSubmissions.stories.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import NoSubmissions from '../components/NoSubmissions';
 import '../../core/styles/index.scss';
 
 storiesOf('Dashboard | Components/NoSubmission', module).add(
     'NoSubmissions',
     (): JSX.Element => {
-        return <NoSubmissions onStartClick={action('Start submission clicked')} />;
+        return <NoSubmissions />;
     },
 );
